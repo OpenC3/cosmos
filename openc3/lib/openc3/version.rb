e14161e03c5fd934be532e3006a1e0d8860b0c72
ryanmelt <ryanmelt@users.noreply.github.com>
2022-07-29 17:32:26 +0000

    [Github Action] Update version to 5.0.6

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -7,7 +7,7 @@ module OpenC3
     MINOR = '0'
     PATCH = '6'
     OTHER = ''
-    BUILD = '42febf136cf5f6f0b06adce3b4815a372843b956'
+    BUILD = 'a855874ec4d38adb28e024dd53f9094b9d0c4574'
   end
   VERSION = '5.0.6'
   GEM_VERSION = '5.0.6'
