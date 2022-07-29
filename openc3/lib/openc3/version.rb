8ac2d0e915657acd2f8b00d09045f67abb9dd96c
Ryan Melton <ryan@openc3.com>
2022-07-29 14:29:08 -0600

    Bump version

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -1,14 +1,14 @@
 # encoding: ascii-8bit
 
-OPENC3_VERSION = '5.0.6'
+OPENC3_VERSION = '5.0.7-beta0'
 module OpenC3
   module Version
     MAJOR = '5'
     MINOR = '0'
-    PATCH = '6'
-    OTHER = ''
-    BUILD = 'a855874ec4d38adb28e024dd53f9094b9d0c4574'
+    PATCH = '7'
+    OTHER = 'pre.beta0'
+    BUILD = 'e14161e03c5fd934be532e3006a1e0d8860b0c72'
   end
-  VERSION = '5.0.6'
-  GEM_VERSION = '5.0.6'
+  VERSION = '5.0.7-beta0'
+  GEM_VERSION = '5.0.7.pre.beta0'
 end
