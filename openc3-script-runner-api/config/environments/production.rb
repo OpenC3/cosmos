6ef518ff2bdc189b0de70eece6cc84b10b139bd4
Ryan Melton <ryan@openc3.com>
2022-09-01 15:57:54 -0600

    remove obsolete commented out sections

--- openc3-script-runner-api/config/environments/production.rb
+++ openc3-script-runner-api/config/environments/production.rb
@@ -72,10 +72,4 @@ Rails.application.configure do
 
   # Disable ActionCable logging
   ActionCable.server.config.logger = Logger.new(STDOUT, level: Logger::ERROR)
-
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
 end
