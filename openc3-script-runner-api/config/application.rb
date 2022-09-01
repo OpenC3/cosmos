60c027087b560830af09f72fab5e544c0fec7e8e
Jason Thomas <jason@openc3.com>
2022-09-01 11:19:34 -0600

    Upgrade to rails 7

--- openc3-script-runner-api/config/application.rb
+++ openc3-script-runner-api/config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -12,7 +12,6 @@ require "action_controller/railtie"
 # require "action_text/engine"
 # require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -22,14 +21,17 @@ Bundler.require(*Rails.groups)
 module ScriptRunnerApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
     # Add to hosts to prevent "Blocked host" errors
     config.hosts.clear
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
