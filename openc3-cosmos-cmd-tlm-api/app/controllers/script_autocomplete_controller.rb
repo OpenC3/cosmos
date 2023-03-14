260ed1744558bdc951e6fa50c3439e371b6a60ee
Jason Thomas <jason@openc3.com>
2023-03-14 17:20:40 -0600

    Fix screen keywords, add choice selections, add SETTING

--- openc3-cosmos-cmd-tlm-api/app/controllers/script_autocomplete_controller.rb
+++ openc3-cosmos-cmd-tlm-api/app/controllers/script_autocomplete_controller.rb
@@ -61,7 +61,8 @@ class ScriptAutocompleteController < ApplicationController
     yaml.each do |keyword, data|
       if data['collection']
         keywords.concat(data['collection'].keys)
-      else
+      end
+      if keyword == keyword.upcase
         keywords << keyword
       end
     end
@@ -69,74 +70,106 @@ class ScriptAutocompleteController < ApplicationController
   end
 
   def build_autocomplete_data(type, scope)
+    targets = OpenC3::TargetModel.all(scope: scope)
+    @target_names = targets.keys
+    autocomplete_data = []
     if type.upcase == 'SCREEN'
       yaml = OpenC3::MetaConfigParser.load(File.join(OpenC3::PATH, 'data', 'config', 'screen.yaml'))
-      yaml.map.each do |keyword, data|
+      yaml.each do |keyword, data|
         if data['collection']
           data['collection'].each do |keyword, data|
-            screen_to_autocomplete_hash(keyword, data)
+            screen_to_autocomplete_hash(autocomplete_data, keyword, data)
           end
-        else
-          screen_to_autocomplete_hash(keyword, data)
+        end
+        if keyword == keyword.upcase
+          screen_to_autocomplete_hash(autocomplete_data, keyword, data)
         end
       end
     else
-      autocomplete_data = OpenC3::TargetModel.all(scope: scope).flat_map do |target_name, target_info|
-        OpenC3::TargetModel.packets(target_name, type: type.upcase.intern, scope: scope).flat_map do |packet|
-          packet_to_autocomplete_hashes(packet, target_info, type)
+      targets.each do |target_name, target_info|
+        OpenC3::TargetModel.packets(target_name, type: type.upcase.intern, scope: scope).each do |packet|
+          packet_to_autocomplete_hashes(autocomplete_data, packet, target_info, type)
         end
       end
       autocomplete_data.sort_by { |packet| packet[:caption] }
     end
+    autocomplete_data
   end
 
-  def screen_to_autocomplete_hash(keyword, data)
-    params = []
-    if data['parameters']
-      params = data['parameters'].collect { |param| param['name'] }
-    end
+  def screen_to_autocomplete_hash(autocomplete_data, keyword, data)
     # The snippet is what gets put in the file when you autocomplete
     # Thus we put the keyword with all the parameters surround by <>
     # e.g. SCREEN <Width> <Height> <Polling Period>
+    # Example of building snippets:
+    # https://github.com/ajaxorg/ace/blob/23c3c6067ed55518b4560214f35b5f03378c441a/src/snippets_test.js
     snippet = keyword.dup
-    params.each_with_index.map do |item, index|
-      # map to Ace autocomplete data syntax to allow tabbing through items: "${position:defaultValue}"
-      snippet << " ${#{index + 1}:<#{item}>}"
+
+    params = []
+    if data['parameters']
+      data['parameters'].each_with_index do |param, index|
+        # map to Ace autocomplete data syntax to allow tabbing through items: "${position:defaultValue}"
+        if param['values'].is_a? Array
+          snippet << " ${#{index + 1}|#{param['values'].join(',')}|}"
+        elsif param['name'] == "Target name"
+          snippet << " ${#{index + 1}|#{@target_names.join(',')}|}"
+        # TODO: "Packet name" based on Target name?
+        # See https://github.com/ajaxorg/ace/discussions/5089
+        else
+          snippet << " ${#{index + 1}:<#{param['name']}>}"
+        end
+      end
+      # Automatically add all the settings so they're aware ... they can delete later
+      if data['settings']
+        data['settings'].each do |key, value|
+          if value['parameters']
+            snippet << "\n  SETTING #{key} " +
+              value['parameters'].map {|param| "<#{param['name']}>" }.join(' ')
+          else
+            snippet << "\n  SETTING #{key}"
+          end
+        end
+      end
+    elsif data['collection']
+      # Create option drop down list
+      snippet << " ${1|#{data['collection'].keys.join(',')}|}"
     end
-    {
-      :caption => keyword,
-      :snippet => snippet,
-      :meta => data['summary'],
-    }
+    autocomplete_data <<
+      {
+        :caption => keyword,
+        :snippet => snippet,
+        :meta => data['summary']
+      }
   end
 
   def target_packet_name(packet)
     "#{packet['target_name']} #{packet['packet_name']}"
   end
 
-  def packet_to_autocomplete_hashes(packet, target_info, type)
+  def packet_to_autocomplete_hashes(autocomplete_data, packet, target_info, type)
     if type.upcase == 'TLM'
-      return packet['items'].map do |item|
-        {
-          :caption => "#{target_packet_name(packet)} #{item['name']}",
-          :snippet => "#{target_packet_name(packet)} #{item['name']}",
-          :meta => 'telemetry',
-        }
+      packet['items'].each do |item|
+        autocomplete_data <<
+          {
+            :caption => "#{target_packet_name(packet)} #{item['name']}",
+            :snippet => "#{target_packet_name(packet)} #{item['name']}",
+            :meta => 'telemetry',
+          }
       end
     else
       # There's only one autocomplete option for each command packet
-      return [
+      autocomplete_data <<
         {
           :caption => target_packet_name(packet),
           :snippet => build_cmd_snippet(packet, target_info),
           :meta => 'command',
         }
-      ]
     end
   end
 
+  # Example of building snippets:
+  # https://github.com/ajaxorg/ace/blob/23c3c6067ed55518b4560214f35b5f03378c441a/src/snippets_test.js
   def build_cmd_snippet(packet, target_info)
-    caption = target_packet_name(packet)
+    snippet = target_packet_name(packet)
     filtered_items = packet['items'].select do |item|
       !OpenC3::Packet::RESERVED_ITEM_NAMES.include?(item['name']) and !target_info['ignored_parameters'].include?(item['name'])
     end
@@ -150,8 +183,8 @@ class ScriptAutocompleteController < ApplicationController
         # map to Ace autocomplete data syntax to allow tabbing through items: "staticText ${position:defaultValue}"
         "#{item['name']} ${#{index + 1}:#{default}}"
       end
-      return "#{caption} with #{params.join(', ')}"
+      return "#{snippet} with #{params.join(', ')}"
     end
-    caption
+    snippet
   end
 end
---
+++ openc3-cosmos-cmd-tlm-api/spec/controllers/script_autocomplete_controller_spec.rb
@@ -0,0 +1,89 @@
+# encoding: ascii-8bit
+
+# Copyright 2023 OpenC3, Inc.
+# All Rights Reserved.
+#
+# This program is free software; you can modify and/or redistribute it
+# under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation; version 3 with
+# attribution addendums as found in the LICENSE.txt
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# This file may also be used under the terms of a commercial license
+# if purchased from OpenC3, Inc.
+
+require 'rails_helper'
+require 'openc3/models/target_model'
+
+RSpec.describe ScriptAutocompleteController, :type => :controller do
+  before(:each) do
+    mock_redis()
+    setup_system()
+    model = OpenC3::TargetModel.new(folder_name: "INST", name: "INST", scope: "DEFAULT")
+    model.create
+    dir = File.join(__dir__, '..', '..', '..', 'openc3', 'spec', 'install', 'config', 'targets')
+    model.update_store(OpenC3::System.new(['INST'], dir))
+  end
+
+  describe "get_keywords" do
+    it "gets screen keywords" do
+      get :get_keywords, params: { type: 'SCREEN' }
+      expect(response).to have_http_status(:success)
+      ret = JSON.parse(response.body, :allow_nan => true, :create_additions => true)
+      # Spot check
+      expect(ret).to include("SCREEN")
+      expect(ret).to include("END")
+      expect(ret).to include("END")
+      expect(ret).to include("LABEL")
+      expect(ret).to include("VALUE")
+      expect(ret).to include("VERTICAL")
+    end
+  end
+
+  describe "get_ace_autocomplete_data" do
+    it "gets screen autocomplete" do
+      get :get_ace_autocomplete_data, params: { type: 'SCREEN', scope: 'DEFAULT' }
+      expect(response).to have_http_status(:success)
+      ret = JSON.parse(response.body, :allow_nan => true, :create_additions => true)
+      # Spot check
+      expect(ret).to include({"caption"=>"SCREEN",
+        "snippet"=>"SCREEN ${1:<Width>} ${2:<Height>} ${3:<Polling Period>}",
+        "meta"=>"Define a telemetry viewer screen"})
+      expect(ret).to include({"caption"=>"CANVAS",
+        "snippet"=>"CANVAS ${1:<Width>} ${2:<Height>}",
+        "meta"=>"Layout widget for the other canvas widgets"})
+      expect(ret).to include({"caption"=>"VALUE",
+        # Packet names as options ${1|INST|} and type as options ${4|RAW,CONVERTED,FORMATTED,WITH_UNITS|}
+        "snippet"=>"VALUE ${1|INST|} ${2:<Packet name>} ${3:<Item name>} ${4|RAW,CONVERTED,FORMATTED,WITH_UNITS|} ${5:<Number of characters>}",
+        "meta"=>"Displays a box with a telemetry item value"})
+      end
+
+    it "gets cmd autocomplete" do
+      get :get_ace_autocomplete_data, params: { type: 'CMD', scope: 'DEFAULT' }
+      expect(response).to have_http_status(:success)
+      ret = JSON.parse(response.body, :allow_nan => true, :create_additions => true)
+      # Sorted so first should be INST ABORT
+      expect(ret[0]).to include({"caption" => "INST ABORT"})
+      # Spot check
+      expect(ret).to include({"caption"=>"INST COLLECT",
+        "snippet"=>"INST COLLECT with TYPE ${1:NORMAL}, DURATION ${2:1.0}, OPCODE ${3:171}, TEMP ${4:0.0}",
+        "meta"=>"command"})
+    end
+
+    it "gets tlm autocomplete" do
+      get :get_ace_autocomplete_data, params: { type: 'TLM', scope: 'DEFAULT' }
+      expect(response).to have_http_status(:success)
+      ret = JSON.parse(response.body, :allow_nan => true, :create_additions => true)
+      # Sorted so first should be INST ADCS CCSDSVER
+      expect(ret[0]).to include({"caption" => "INST ADCS CCSDSVER"})
+      # Spot check
+      expect(ret).to include({"caption"=>"INST HEALTH_STATUS DURATION",
+        "snippet"=>"INST HEALTH_STATUS DURATION",
+        "meta"=>"telemetry"})
+    end
+  end
+end
