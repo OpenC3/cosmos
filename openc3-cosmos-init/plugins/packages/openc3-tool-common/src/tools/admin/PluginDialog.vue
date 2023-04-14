597e4846a8be0f507f5326835bc58ec456ea5bc5
Jason Thomas <jason@openc3.com>
2023-04-14 12:17:16 -0600

    Make tabs eager to load ACE

--- openc3-cosmos-init/plugins/packages/openc3-tool-common/src/tools/admin/PluginDialog.vue
+++ openc3-cosmos-init/plugins/packages/openc3-tool-common/src/tools/admin/PluginDialog.vue
@@ -46,7 +46,7 @@
         </v-row>
         <form v-on:submit.prevent="submit">
           <v-tabs-items v-model="tab">
-            <v-tab-item :key="0">
+            <v-tab-item :key="0" eager="true">
               <div class="pa-3">
                 <v-row class="mt-3">
                   <div v-for="(value, name) in localVariables" :key="name">
@@ -62,14 +62,17 @@
                 </v-row>
               </div>
             </v-tab-item>
-            <v-tab-item v-if="existingPluginTxt === null" :key="1">
+            <v-tab-item v-if="existingPluginTxt === null" :key="1" eager="true">
+              <v-row
+                ><v-col>This can be edited before installation.</v-col></v-row
+              >
               <pre id="editor"></pre>
             </v-tab-item>
-            <v-tab-item v-else :key="1">
+            <v-tab-item v-else :key="1" eager="true">
               <v-row
                 ><v-col
-                  >Existing plugin.txt. This can be edited and will be
-                  installed.</v-col
+                  >Existing plugin.txt. This can be edited before
+                  installation.</v-col
                 ><v-col
                   >Uneditable plugin.txt from the new plugin.</v-col
                 ></v-row
@@ -143,6 +146,38 @@ export default {
       differ: null,
     }
   },
+  mounted() {
+    const pluginMode = this.buildPluginMode()
+    if (this.existingPluginTxt === null) {
+      this.editor = ace.edit('editor')
+      this.editor.setTheme('ace/theme/twilight')
+      this.editor.session.setMode(new pluginMode())
+      this.editor.session.setTabSize(2)
+      this.editor.session.setUseWrapMode(true)
+      this.editor.$blockScrolling = Infinity
+      this.editor.setHighlightActiveLine(false)
+      this.editor.setValue(this.localPluginTxt)
+      this.editor.clearSelection()
+      this.editor.focus()
+    } else {
+      this.differ = new AceDiff({
+        element: '#acediff',
+        mode: new pluginMode(),
+        theme: 'ace/theme/twilight',
+        left: {
+          content: this.localExistingPluginTxt,
+          copyLinkEnabled: false,
+        },
+        right: {
+          content: this.localPluginTxt,
+          editable: false,
+        },
+      })
+      // Match our existing editors
+      this.differ.getEditors().left.setFontSize(16)
+      this.differ.getEditors().right.setFontSize(16)
+    }
+  },
   beforeDestroy() {
     if (this.editor) {
       this.editor.destroy()
@@ -171,43 +206,6 @@ export default {
           this.localExistingPluginTxt = this.existingPluginTxt.slice()
         }
       },
-    },
-    tab: async function (newVal, oldVal) {
-      if (newVal === 1) {
-        if (this.existingPluginTxt === null && !this.editor) {
-          const openPluginMode = this.buildPluginMode()
-          await new Promise((r) => setTimeout(r, 300))
-          this.editor = ace.edit('editor')
-          this.editor.setTheme('ace/theme/twilight')
-          this.editor.session.setMode(new openPluginMode())
-          this.editor.session.setTabSize(2)
-          this.editor.session.setUseWrapMode(true)
-          this.editor.$blockScrolling = Infinity
-          this.editor.setHighlightActiveLine(false)
-          this.editor.setValue(this.localPluginTxt)
-          this.editor.clearSelection()
-          this.editor.focus()
-        } else if (this.existingPluginTxt !== null && !this.differ) {
-          const pluginMode = this.buildPluginMode()
-          await new Promise((r) => setTimeout(r, 300))
-          this.differ = new AceDiff({
-            element: '#acediff',
-            mode: new pluginMode(),
-            theme: 'ace/theme/twilight',
-            left: {
-              content: this.localExistingPluginTxt,
-              copyLinkEnabled: false,
-            },
-            right: {
-              content: this.localPluginTxt,
-              editable: false,
-            },
-          })
-          // Match our existing editors
-          this.differ.getEditors().left.setFontSize(16)
-          this.differ.getEditors().right.setFontSize(16)
-        }
-      }
     },
   },
   methods: {
