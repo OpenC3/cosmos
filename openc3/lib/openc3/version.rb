7f48c2494ed6c19570aad03946cc4ef3fe6e8977
ryanmelt <ryanmelt@users.noreply.github.com>
2023-04-21 06:03:17 +0000

    [Github Action] Update version to 5.6.2-beta0

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -1,14 +1,14 @@
 # encoding: ascii-8bit
 
-OPENC3_VERSION = '5.6.1'
+OPENC3_VERSION = '5.6.2-beta0'
 module OpenC3
   module Version
     MAJOR = '5'
     MINOR = '6'
-    PATCH = '1'
-    OTHER = ''
-    BUILD = '86a8498603e3f90548f20613cb5b596c906f2a9e'
+    PATCH = '2'
+    OTHER = 'pre.beta0'
+    BUILD = '580186cb1e60d18d88b3ec0418ceeb00c178ff67'
   end
-  VERSION = '5.6.1'
-  GEM_VERSION = '5.6.1'
+  VERSION = '5.6.2-beta0'
+  GEM_VERSION = '5.6.2.pre.beta0'
 end
