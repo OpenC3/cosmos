7f5e4d9e6add7d51cbd61ad58fec2141c819bc81
Jason Thomas <jason@openc3.com>
2023-05-10 15:02:36 -0600

    RubyLex with 3.1 and 3.2

--- openc3/lib/openc3/utilities/ruby_lex_utils.rb
+++ openc3/lib/openc3/utilities/ruby_lex_utils.rb
@@ -41,6 +41,14 @@ class RubyLex
     @prompt = nil
     initialize_input()
   end
+
+  major, minor, patch = RUBY_VERSION.split('.')
+  if major == '3' and minor.to_i < 2
+    alias orig_lex lex
+    def lex(context)
+      orig_lex()
+    end
+  end
 end
 $VERBOSE = old_verbose
 
