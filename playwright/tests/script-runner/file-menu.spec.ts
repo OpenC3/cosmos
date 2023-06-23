9fefbf8035ebf07073dfb61565478e7c4134cf23
Jason Thomas <jason@openc3.com>
2023-06-23 16:37:01 -0600

    Move playwright into main repo

---
+++ playwright/tests/script-runner/file-menu.spec.ts
@@ -0,0 +1,202 @@
+/*
+# Copyright 2022 Ball Aerospace & Technologies Corp.
+# All Rights Reserved.
+#
+# This program is free software; you can modify and/or redistribute it
+# under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation; version 3 with
+# attribution addendums as found in the LICENSE.txt
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# Modified by OpenC3, Inc.
+# All changes Copyright 2022, OpenC3, Inc.
+# All Rights Reserved
+*/
+
+// @ts-check
+import { test, expect } from './../fixture'
+
+test.use({
+  toolPath: '/tools/scriptrunner',
+  toolName: 'Script Runner',
+})
+
+test('clears the editor on File->New', async ({ page, utils }) => {
+  // Have to fill on an editable area like the textarea
+  await page.locator('textarea').fill('this is a test')
+  // But can't check on the textarea because it has an input
+  await expect(page.locator('.editor')).toContainText('this is a test')
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await page.locator('text=New File').click()
+  await expect(page.locator('.editor')).not.toContainText('this is a test')
+})
+
+test('open a file', async ({ page, utils }) => {
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await page.locator('text=Open File').click()
+  await utils.sleep(1000)
+  await page.locator('[data-test=file-open-save-search]').type('dis')
+  await utils.sleep(500)
+  await page.locator('[data-test=file-open-save-search]').type('connect')
+  await page.locator('text=disconnect >> nth=0').click() // nth=0 because INST, INST2
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  expect(await page.locator('#sr-controls')).toContainText(
+    `INST/procedures/disconnect.rb`
+  )
+})
+
+test('handles File->Save new file', async ({ page, utils }) => {
+  await page.locator('textarea').fill('puts "File Save new File"')
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await page.locator('text=Save File').click()
+  // New files automatically open File Save As
+  await page.locator('text=File Save As')
+  await page.locator('[data-test=file-open-save-filename]').fill('save_new.rb')
+  await page.locator('text=temp.rb is not a valid filename')
+  await page
+    .locator(
+      '.v-dialog >> .v-treeview-node__root:has-text("INST") > button >> nth=0'
+    )
+    .click()
+  await page.locator('text=procedures').click()
+  await page.locator('[data-test=file-open-save-filename]').click()
+  await page.type('[data-test=file-open-save-filename]', '/save_new.rb')
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  expect(await page.locator('#sr-controls')).toContainText(
+    'INST/procedures/save_new.rb'
+  )
+
+  // Delete the file
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await page.locator('text=Delete File').click()
+  await page.locator('text=Permanently delete file')
+  await page.locator('button:has-text("Delete")').click()
+})
+
+test('handles File Save overwrite', async ({ page, utils }) => {
+  await page.locator('textarea').fill('puts "File Save overwrite"')
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await page.locator('text=Save File').click()
+  await page
+    .locator('[data-test=file-open-save-filename]')
+    .fill('INST/procedures/save_overwrite.rb')
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  expect(await page.locator('#sr-controls')).toContainText(
+    'INST/procedures/save_overwrite.rb'
+  )
+
+  await page.locator('textarea').fill('# comment1')
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await page.locator('text=Save File').click()
+  await page.locator('textarea').fill('# comment2')
+  if (process.platform === 'darwin') {
+    await page.locator('textarea').press('Meta+S') // Ctrl-S save
+  } else {
+    await page.locator('textarea').press('Control+S') // Ctrl-S save
+  }
+
+  // File->Save As
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await page.locator('text=Save As...').click()
+  await page.locator('text=INST/procedures/save_overwrite.rb')
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  // Confirmation dialog
+  await page.locator('text=Are you sure you want to overwrite').click()
+  await page.locator('button:has-text("Overwrite")').click()
+
+  // Delete the file
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await page.locator('text=Delete File').click()
+  await page.locator('text=Permanently delete file')
+  await page.locator('button:has-text("Delete")').click()
+})
+
+test('handles Download', async ({ page, utils }) => {
+  await page.locator('textarea').fill('download this')
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await page.locator('text=Save File').click()
+  await page.fill('[data-test=file-open-save-filename]', 'INST/download.txt')
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  expect(await page.locator('#sr-controls')).toContainText('INST/download.txt')
+  // Download the file
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await utils.download(
+    page,
+    '[data-test=cosmos-script-runner-file-download]',
+    function (contents) {
+      expect(contents).toContain('download this')
+    }
+  )
+
+  // Delete the file
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await page.locator('text=Delete File').click()
+  await page.locator('text=Permanently delete file')
+  await page.locator('button:has-text("Delete")').click()
+})
+
+test('can delete all temp files', async ({ page, utils }) => {
+  // Create new file which when run will become a TEMP file
+  await page.locator('textarea').fill('puts "temp11111111"')
+  await page.locator('[data-test=start-button]').click()
+  await expect(page.locator('[data-test=state]')).toHaveValue('Connecting...', {
+    timeout: 5000,
+  })
+  await expect(page.locator('[data-test=state]')).toHaveValue('stopped', {
+    timeout: 20000,
+  })
+  await expect(page.locator('#sr-controls')).toContainText(
+    /__TEMP__\/\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}_\d{3}_temp.rb/
+  )
+  // Weird selector but it's how vuetify works
+  let tempFile1 = await page
+    .locator('#sr-controls >> input[type=hidden]')
+    .inputValue()
+  tempFile1 = tempFile1.split('/')[1]
+
+  // New file
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await page.locator('text=New File').click()
+  await expect(page.locator('#sr-controls')).toContainText('<Untitled>')
+  await page.locator('textarea').fill('puts "temp22222222"')
+  await page.locator('[data-test=start-button]').click()
+  await expect(page.locator('[data-test=state]')).toHaveValue('Connecting...', {
+    timeout: 5000,
+  })
+  await expect(page.locator('[data-test=state]')).toHaveValue('stopped', {
+    timeout: 20000,
+  })
+  await expect(page.locator('#sr-controls')).toContainText(
+    /__TEMP__\/\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}_\d{3}_temp.rb/
+  )
+  let tempFile2 = await page
+    .locator('#sr-controls >> input[type=hidden]')
+    .inputValue()
+  tempFile2 = tempFile2.split('/')[1]
+  expect(tempFile1).not.toEqual(tempFile2)
+
+  // Open file
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await page.locator('text=Open File').click()
+  await page.locator('.v-dialog >> text=__TEMP__').click()
+  await expect(page.locator(`.v-dialog >> text=${tempFile1}`)).toBeVisible()
+  await expect(page.locator(`.v-dialog >> text=${tempFile2}`)).toBeVisible()
+
+  await page
+    .locator('.v-dialog >> .v-treeview-node:has-text("__TEMP__") >> .v-btn')
+    .click()
+  await page.locator('[data-test="confirm-dialog-delete"]').click()
+  await utils.sleep(300) // Allow confirm to complete
+  await page.locator('[data-test="file-open-save-cancel-btn"]').click()
+
+  // Open file
+  await page.locator('[data-test=cosmos-script-runner-file]').click()
+  await page.locator('text=Open File').click()
+  await expect(page.locator('.v-dialog--active')).toContainText('INST')
+  await expect(page.locator('.v-dialog--active')).not.toContainText('__TEMP__')
+  await page.locator('[data-test="file-open-save-cancel-btn"]').click()
+})
---
+++ playwright/tests/table-manager.spec.ts
@@ -0,0 +1,375 @@
+/*
+# Copyright 2022 Ball Aerospace & Technologies Corp.
+# All Rights Reserved.
+#
+# This program is free software; you can modify and/or redistribute it
+# under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation; version 3 with
+# attribution addendums as found in the LICENSE.txt
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# Modified by OpenC3, Inc.
+# All changes Copyright 2022, OpenC3, Inc.
+# All Rights Reserved
+*/
+
+// @ts-check
+import { test, expect } from './fixture'
+
+test.use({
+  toolPath: '/tools/tablemanager',
+  toolName: 'Table Manager',
+})
+
+//
+// Test the File menu
+//
+test('creates a single binary file', async ({ page, utils }) => {
+  await page.locator('[data-test=cosmos-table-manager-file]').click()
+  await page.locator('text=New').click()
+  await expect(page.locator('.v-dialog')).toBeVisible()
+  await utils.sleep(500) // Allow file dialog to fully render
+  await page.locator('[data-test="file-open-save-search"]').fill('MCConfig')
+  await page.locator('text=MCConfig >> nth=0').click() // nth=0 because INST, INST2
+  await page.locator('[data-test="file-open-save-submit-btn"]').click()
+  await expect(page.locator('id=openc3-tool')).toContainText('MC_CONFIGURATION')
+  expect(await page.locator('.v-tab')).toHaveCount(1)
+  expect(await page.inputValue('[data-test=definition-filename]')).toMatch(
+    'INST/tables/config/MCConfigurationTable_def.txt'
+  )
+  expect(await page.inputValue('[data-test=filename]')).toMatch(
+    'INST/tables/bin/MCConfigurationTable.bin'
+  )
+})
+
+test('edits a binary file', async ({ page, utils }) => {
+  await page.locator('[data-test=cosmos-table-manager-file]').click()
+  await page.locator('text=New').click() // Create new since we're editing
+  await expect(page.locator('.v-dialog')).toBeVisible()
+  await utils.sleep(500) // Allow file dialog to fully render
+  await page.locator('[data-test=file-open-save-search]').type('ConfigTables_')
+  await page.locator('text=ConfigTables_ >> nth=0').click() // nth=0 because INST, INST2
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  await expect(page.locator('id=openc3-tool')).toContainText('MC_CONFIGURATION')
+  await expect(page.locator('id=openc3-tool')).toContainText('TLM_MONITORING')
+  await expect(page.locator('id=openc3-tool')).toContainText('PPS_SELECTION')
+  expect(await page.locator('.v-tab')).toHaveCount(3)
+  expect(
+    await page.locator('[data-test=definition-filename]').inputValue()
+  ).toMatch('INST/tables/config/ConfigTables_def.txt')
+  expect(await page.locator('[data-test=filename]').inputValue()).toMatch(
+    'INST/tables/bin/ConfigTables.bin'
+  )
+
+  // Verify original contents
+  await utils.download(
+    page,
+    '[data-test=download-file-report]',
+    function (contents) {
+      expect(contents).toContain('ConfigTables.bin')
+      expect(contents).toContain('MC_CONFIGURATION')
+      expect(contents).toContain('SCRUB_REGION_1_START_ADDR, 0x0')
+      expect(contents).toContain('TLM_MONITORING')
+      expect(contents).toContain(
+        '1, 0, 0, BITS, 0, 0, LESS_THAN, NO_ACTION_REQUIRED, ALL_MODES, UNSIGNED'
+      )
+      expect(contents).toContain('PPS_SELECTION')
+      expect(contents).toContain('PRIMARY_PPS, CHECKED')
+      expect(contents).toContain('REDUNDANT_PPS, UNCHECKED')
+    }
+  )
+
+  await page.locator('text=MC_CONFIGURATION').click()
+  await page
+    .locator('text=1SCRUB_REGION_1_START_ADDR >> input[type="text"]')
+    .fill('0xabcdef')
+
+  await page.locator('text=TLM_MONITORING').click()
+  await expect(page.locator('id=openc3-tool')).toContainText('THRESHOLD')
+  await page
+    .locator(
+      '[data-test=TLM_MONITORING] tr:nth-child(1) td:nth-child(2) [data-test=table-item-text-field]'
+    )
+    .fill('1')
+  await page
+    .locator(
+      '[data-test=TLM_MONITORING] tr:nth-child(1) td:nth-child(3) [data-test=table-item-text-field]'
+    )
+    .fill('2')
+  await page
+    .locator('[data-test=TLM_MONITORING] tr:nth-child(1)')
+    .getByRole('button', { name: 'BITS' })
+    .click()
+  await page.locator('text=BYTE').click()
+  await expect(
+    page.locator('[data-test=TLM_MONITORING] tr:nth-child(1) td:nth-child(4)')
+  ).toContainText('BYTE')
+  await page
+    .locator(
+      '[data-test=TLM_MONITORING] tr:nth-child(1) td:nth-child(5) [data-test=table-item-text-field]'
+    )
+    .fill('3')
+  await page
+    .locator(
+      '[data-test=TLM_MONITORING] tr:nth-child(1) td:nth-child(6) [data-test=table-item-text-field]'
+    )
+    .fill('4')
+  await page
+    .locator('[data-test=TLM_MONITORING] tr:nth-child(1)')
+    .getByRole('button', { name: 'LESS_THAN' })
+    .click()
+  await page.locator('text=GREATER_THAN').click()
+  await expect(
+    page.locator('[data-test=TLM_MONITORING] tr:nth-child(1) td:nth-child(7)')
+  ).toContainText('GREATER_THAN')
+  await page
+    .locator('[data-test=TLM_MONITORING] tr:nth-child(1)')
+    .getByRole('button', { name: 'NO_ACTION_REQUIRED' })
+    .click()
+  await page.locator('text=INITIATE_RESET').click()
+  await expect(
+    page.locator('[data-test=TLM_MONITORING] tr:nth-child(1) td:nth-child(8)')
+  ).toContainText('INITIATE_RESET')
+  await page
+    .locator('[data-test=TLM_MONITORING] tr:nth-child(1)')
+    .getByRole('button', { name: 'ALL_MODES' })
+    .click()
+  await page.locator('text=SAFE_MODE').click()
+  await expect(
+    page.locator('[data-test=TLM_MONITORING] tr:nth-child(1) td:nth-child(9)')
+  ).toContainText('SAFE_MODE')
+
+  await page.locator('text=PPS_SELECTION').click()
+  await page.locator('text=1PRIMARY_PPS >> div').nth(4).click()
+  await page.locator('text=2REDUNDANT_PPS >> div').nth(4).click()
+
+  await page.locator('[data-test=cosmos-table-manager-file]').click()
+  await page.locator('text=Save File').click()
+  await utils.sleep(5000) // Saving takes some time
+
+  // Check for new values
+  await utils.download(
+    page,
+    '[data-test=download-file-report]',
+    function (contents) {
+      expect(contents).toContain('ConfigTables.bin')
+      expect(contents).toContain('MC_CONFIGURATION')
+      expect(contents).toContain('SCRUB_REGION_1_START_ADDR, 0xABCDEF')
+      expect(contents).toContain('TLM_MONITORING')
+      expect(contents).toContain(
+        '1, 1, 2, BYTE, 3, 4, GREATER_THAN, INITIATE_RESET, SAFE_MODE'
+      )
+      expect(contents).toContain('PPS_SELECTION')
+      expect(contents).toContain('PRIMARY_PPS, UNCHECKED')
+      expect(contents).toContain('REDUNDANT_PPS, CHECKED')
+    }
+  )
+})
+
+test('opens and searches file', async ({ page, utils }) => {
+  await page.locator('[data-test=cosmos-table-manager-file]').click()
+  await page.locator('text=Open File').click()
+  await utils.sleep(500) // Allow file dialog to fully render
+  await page
+    .locator('[data-test=file-open-save-search]')
+    .type('ConfigTables.bin')
+  await page.locator('text=ConfigTables >> nth=0').click()
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  await expect(page.locator('id=openc3-tool')).toContainText('MC_CONFIGURATION')
+  await expect(page.locator('id=openc3-tool')).toContainText('TLM_MONITORING')
+  await expect(page.locator('id=openc3-tool')).toContainText('PPS_SELECTION')
+  expect(await page.locator('.v-tab')).toHaveCount(3)
+  expect(
+    await page.locator('[data-test=definition-filename]').inputValue()
+  ).toMatch('INST/tables/config/ConfigTables_def.txt')
+  expect(await page.locator('[data-test=filename]').inputValue()).toMatch(
+    'INST/tables/bin/ConfigTables.bin'
+  )
+
+  // Test searching
+  expect(await page.locator('tr')).toHaveCount(12)
+  await page.locator('text=Items >> input').fill('UNEDIT')
+  await expect.poll(() => page.locator('tr').count()).toBe(4)
+  // Vuetify sets the disabled attribute to disabled so just check for that
+  // Checking for toBeDisabled() does not work since the aria-disabled is not set
+  // See https://github.com/microsoft/playwright/issues/13583
+  await expect(
+    page.locator('tr >> input[disabled=disabled] >> nth=0')
+  ).toBeVisible()
+  await expect(
+    page.locator('tr >> input[disabled=disabled] >> nth=1')
+  ).toBeVisible()
+  await expect(
+    page.locator('tr >> input[disabled=disabled] >> nth=2')
+  ).toBeVisible()
+  await page.locator('text=Items >> input').fill('')
+  await expect.poll(() => page.locator('tr').count()).toBe(12)
+})
+
+test('downloads binary, definition, report', async ({ page, utils }) => {
+  await page.locator('[data-test=cosmos-table-manager-file]').click()
+  await page.locator('text=Open File').click()
+  await utils.sleep(500) // Allow file dialog to fully render
+  await page
+    .locator('[data-test=file-open-save-search]')
+    .type('ConfigTables.bin')
+  await page.locator('text=ConfigTables >> nth=0').click()
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  await utils.download(page, '[data-test=download-file-binary]')
+  await utils.download(
+    page,
+    '[data-test=download-file-definition]',
+    function (contents) {
+      expect(contents).toContain('TABLEFILE')
+    }
+  )
+  await utils.download(
+    page,
+    '[data-test=download-file-report]',
+    function (contents) {
+      expect(contents).toContain('ConfigTables.bin')
+    }
+  )
+  await page.locator('text=PPS_SELECTION').click()
+  await utils.download(
+    page,
+    '[data-test="PPS_SELECTION"] [data-test=download-table-binary]',
+    function (contents) {
+      expect(contents.length).toBe(2)
+    },
+    'binary'
+  )
+  await utils.download(
+    page,
+    '[data-test="PPS_SELECTION"] [data-test=download-table-definition]',
+    function (contents) {
+      expect(contents).toContain('TABLE "PPS_Selection"')
+    }
+  )
+  await utils.download(
+    page,
+    '[data-test="PPS_SELECTION"] [data-test=download-table-report]',
+    function (contents) {
+      expect(contents).toContain('PPS_SELECTION')
+    }
+  )
+})
+
+test('save as', async ({ page, utils }) => {
+  await page.locator('[data-test=cosmos-table-manager-file]').click()
+  await page.locator('text=Open File').click()
+  await expect(page.locator('.v-dialog')).toBeVisible()
+  await utils.sleep(500) // Allow file dialog to fully render
+  await page
+    .locator('[data-test=file-open-save-search]')
+    .type('ConfigTables.bin')
+  await page.locator('text=ConfigTables >> nth=0').click()
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  await expect(page.locator('id=openc3-tool')).toContainText('MC_CONFIGURATION')
+  expect(await page.locator('[data-test=filename]').inputValue()).toMatch(
+    'INST/tables/bin/ConfigTables.bin'
+  )
+  expect(
+    await page.locator('[data-test=definition-filename]').inputValue()
+  ).toMatch('INST/tables/config/ConfigTables_def.txt')
+
+  await page.locator('[data-test=cosmos-table-manager-file]').click()
+  await page.locator('text=Save As').click()
+  await expect(page.locator('.v-dialog')).toBeVisible()
+  await utils.sleep(500) // Allow file dialog to fully render
+  await page
+    .locator('[data-test=file-open-save-filename]')
+    .fill('INST/tables/bin/ConfigTables2.bin')
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  await utils.sleep(1000)
+  expect(await page.locator('[data-test=filename]').inputValue()).toMatch(
+    'INST/tables/bin/ConfigTables2.bin'
+  )
+  expect(
+    await page.locator('[data-test=definition-filename]').inputValue()
+  ).toMatch('INST/tables/config/ConfigTables_def.txt')
+
+  // Verify we can open it cleanly
+  await page.locator('[data-test=cosmos-table-manager-file]').click()
+  await page.locator('text=Open File').click()
+  await expect(page.locator('.v-dialog')).toBeVisible()
+  await utils.sleep(500) // Allow file dialog to fully render
+  await page
+    .locator('[data-test=file-open-save-search]')
+    .type('ConfigTables2.bin')
+  await page.locator('text=ConfigTables >> nth=0').click()
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  await expect(page.locator('id=openc3-tool')).toContainText('MC_CONFIGURATION')
+  expect(await page.locator('[data-test=filename]').inputValue()).toMatch(
+    'INST/tables/bin/ConfigTables2.bin'
+  )
+  expect(
+    await page.locator('[data-test=definition-filename]').inputValue()
+  ).toMatch('INST/tables/config/ConfigTables_def.txt')
+
+  // Save As to something that doesn't match the definition file convention
+  await page.locator('[data-test=cosmos-table-manager-file]').click()
+  await page.locator('text=Save As').click()
+  await expect(page.locator('.v-dialog')).toBeVisible()
+  await utils.sleep(500) // Allow file dialog to fully render
+  await page
+    .locator('[data-test=file-open-save-filename]')
+    .fill('INST/tables/bin/Binary.bin')
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  await utils.sleep(1000)
+  expect(await page.locator('[data-test=filename]').inputValue()).toMatch(
+    'INST/tables/bin/Binary.bin'
+  )
+  expect(
+    await page.locator('[data-test=definition-filename]').inputValue()
+  ).toMatch('INST/tables/config/ConfigTables_def.txt')
+
+  // Now try to open it and be required to select the definition file
+  await page.locator('[data-test=cosmos-table-manager-file]').click()
+  await page.locator('text=Open File').click()
+  await expect(page.locator('.v-dialog')).toBeVisible()
+  await utils.sleep(500) // Allow file dialog to fully render
+  await page.locator('[data-test=file-open-save-search]').type('Binary.bin')
+  await page.locator('text=Binary.bin').click()
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  await utils.sleep(1000) // Allow new file dialog to fully render
+  await page
+    .locator('[data-test=file-open-save-search]')
+    .type('ConfigTables_def')
+  await page.locator('text=ConfigTables_def >> nth=0').click()
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  await expect(page.locator('id=openc3-tool')).toContainText('MC_CONFIGURATION')
+  expect(await page.locator('[data-test=filename]').inputValue()).toMatch(
+    'INST/tables/bin/Binary.bin'
+  )
+  expect(
+    await page.locator('[data-test=definition-filename]').inputValue()
+  ).toMatch('INST/tables/config/ConfigTables_def.txt')
+})
+
+test('delete', async ({ page, utils }) => {
+  await page.locator('[data-test=cosmos-table-manager-file]').click()
+  await page.locator('text=Open File').click()
+  await utils.sleep(500) // Allow file dialog to fully render
+  await page
+    .locator('[data-test=file-open-save-search]')
+    .type('ConfigTables.bin')
+  await page.locator('text=ConfigTables >> nth=0').click()
+  await page.locator('[data-test=file-open-save-submit-btn]').click()
+  await expect(page.locator('id=openc3-tool')).toContainText('MC_CONFIGURATION')
+  expect(await page.locator('[data-test=filename]').inputValue()).toMatch(
+    'INST/tables/bin/ConfigTables.bin'
+  )
+  expect(
+    await page.locator('[data-test=definition-filename]').inputValue()
+  ).toMatch('INST/tables/config/ConfigTables_def.txt')
+
+  await page.locator('[data-test=cosmos-table-manager-file]').click()
+  await page.locator('text=Delete File').click()
+  await page.locator('text=Permanently delete file')
+  await page.locator('button:has-text("Delete")').click()
+})
