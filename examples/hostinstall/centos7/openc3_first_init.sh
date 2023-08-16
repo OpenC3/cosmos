8668378ccb920047ce86af1369c5b4876d5360e5
Jason Thomas <jason@openc3.com>
2023-08-16 09:34:38 -0600

    Fix mc commands

--- examples/hostinstall/centos7/openc3_first_init.sh
+++ examples/hostinstall/centos7/openc3_first_init.sh
@@ -9,13 +9,13 @@ source ./openc3_env.sh
 mc alias set openc3minio "${OPENC3_BUCKET_URL}" ${OPENC3_BUCKET_USERNAME} ${OPENC3_BUCKET_PASSWORD} || exit 1
 
 # Create new canned policy by name script using script-runner.json policy file.
-mc admin policy add openc3minio script $SCRIPT_DIR/../../../openc3-cosmos-init/script-runner.json || exit 1
+mc admin policy create openc3minio script $SCRIPT_DIR/../../../openc3-cosmos-init/script-runner.json || exit 1
 
 # Create a new user scriptrunner on MinIO use mc admin user.
 mc admin user add openc3minio ${OPENC3_SR_BUCKET_USERNAME} ${OPENC3_SR_BUCKET_PASSWORD} || exit 1
 
 # Once the user is successfully created you can now apply the getonly policy for this user.
-mc admin policy set openc3minio script user=${OPENC3_SR_BUCKET_USERNAME} || exit 1
+mc admin policy attach openc3minio script --user=${OPENC3_SR_BUCKET_USERNAME} || exit 1
 
 # Install Plugins
 mkdir -p /tmp/openc3/tmp/tmp
