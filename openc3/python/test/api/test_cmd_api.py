317e98f8239ce45683f07920c255e99253ea75c4
Jason Thomas <jason@openc3.com>
2023-10-17 10:58:03 -0600

    test_cmd_api improvement

--- openc3/python/test/api/test_cmd_api.py
+++ openc3/python/test/api/test_cmd_api.py
@@ -592,19 +592,19 @@ class TestCmdApi(unittest.TestCase):
         result = get_cmd_time("inst", "collect")
         self.assertEqual(result[0], ("INST"))
         self.assertEqual(result[1], ("COLLECT"))
-        self.assertEqual(result[2], int(now))
+        self.assertAlmostEqual(result[2], int(now), delta=1)
         self.assertLess(abs(result[3] - int((now - int(now)) * 1_000_000)), 50000)
 
         result = get_cmd_time("INST")
         self.assertEqual(result[0], ("INST"))
         self.assertEqual(result[1], ("COLLECT"))
-        self.assertEqual(result[2], int(now))
+        self.assertAlmostEqual(result[2], int(now), delta=1)
         self.assertLess(abs(result[3] - int((now - int(now)) * 1_000_000)), 50000)
 
         result = get_cmd_time()
         self.assertEqual(result[0], ("INST"))
         self.assertEqual(result[1], ("COLLECT"))
-        self.assertEqual(result[2], int(now))
+        self.assertAlmostEqual(result[2], int(now), delta=1)
         self.assertLess(abs(result[3] - int((now - int(now)) * 1_000_000)), 50000)
 
         now = time.time()
@@ -613,13 +613,13 @@ class TestCmdApi(unittest.TestCase):
         result = get_cmd_time("INST")
         self.assertEqual(result[0], ("INST"))
         self.assertEqual(result[1], ("ABORT"))  # New latest is ABORT
-        self.assertEqual(result[2], int(now))
+        self.assertAlmostEqual(result[2], int(now), delta=1)
         self.assertLess(abs(result[3] - int((now - int(now)) * 1_000_000)), 50000)
 
         result = get_cmd_time()
         self.assertEqual(result[0], ("INST"))
         self.assertEqual(result[1], ("ABORT"))
-        self.assertEqual(result[2], int(now))
+        self.assertAlmostEqual(result[2], int(now), delta=1)
         self.assertLess(abs(result[3] - int((now - int(now)) * 1_000_000)), 50000)
 
     def test_get_cmd_time_returns_0_if_no_times_are_set(self):
