b38f2c8b5803f73febc7e52d1d56f80df59485bb
Jason Thomas <jason@openc3.com>
2023-11-20 09:40:39 -0700

    Cleanup

--- openc3/python/test/io/test_json_rpc.py
+++ openc3/python/test/io/test_json_rpc.py
@@ -17,31 +17,31 @@
 import unittest
 from unittest.mock import *
 from test.test_helper import *
-from openc3.io.json_rpc import *
+from openc3.io.json_rpc import JsonRpcRequest
 
 
 class TestJsonRpc(unittest.TestCase):
     def test_encodes_non_utf8_params(self):
         json_rpc_request = JsonRpcRequest(0, "cmd", {"DATA": b"\x61\x62"})  # ASCII ab
-        hash = json_rpc_request.to_hash()
-        self.assertEqual(list(hash.keys()), ["jsonrpc", "method", "params", "id"])
-        self.assertEqual(hash["jsonrpc"], "2.0")
-        self.assertEqual(hash["method"], "cmd")
-        self.assertEqual(hash["params"], [{"DATA": "ab"}])  # ASCII
-        self.assertEqual(hash["id"], 0)
+        request = json_rpc_request.to_hash()
+        self.assertEqual(list(request.keys()), ["jsonrpc", "method", "params", "id"])
+        self.assertEqual(request["jsonrpc"], "2.0")
+        self.assertEqual(request["method"], "cmd")
+        self.assertEqual(request["params"], [{"DATA": "ab"}])  # ASCII
+        self.assertEqual(request["id"], 0)
 
         json_rpc_request = JsonRpcRequest(0, "cmd", {"DATA": b"\xc3\xb1"})  # UTF-8
-        hash = json_rpc_request.to_hash()
-        self.assertEqual(hash["params"], [{"DATA": "ñ"}])
+        request = json_rpc_request.to_hash()
+        self.assertEqual(request["params"], [{"DATA": "ñ"}])
 
         json_rpc_request = JsonRpcRequest(0, "cmd", {"DATA": b"\xc3\x28"})
-        hash = json_rpc_request.to_hash()
-        self.assertEqual(hash["params"], [{"DATA": {"raw": [195, 40]}}])
+        request = json_rpc_request.to_hash()
+        self.assertEqual(request["params"], [{"DATA": {"raw": [195, 40]}}])
 
         json_rpc_request = JsonRpcRequest(0, "cmd", {"DATA": b"\xe2\x28\xa1"})
-        hash = json_rpc_request.to_hash()
-        self.assertEqual(hash["params"], [{"DATA": {"raw": [226, 40, 161]}}])
+        request = json_rpc_request.to_hash()
+        self.assertEqual(request["params"], [{"DATA": {"raw": [226, 40, 161]}}])
 
         json_rpc_request = JsonRpcRequest(0, "cmd", {"DATA": b"\xf0\x28\x8c\x28"})
-        hash = json_rpc_request.to_hash()
-        self.assertEqual(hash["params"], [{"DATA": {"raw": [240, 40, 140, 40]}}])
+        request = json_rpc_request.to_hash()
+        self.assertEqual(request["params"], [{"DATA": {"raw": [240, 40, 140, 40]}}])
