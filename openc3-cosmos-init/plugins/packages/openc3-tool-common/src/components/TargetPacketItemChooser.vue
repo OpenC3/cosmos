e9b04245000eb8f9efdd07cbdcf7db3010adffbf
Jason Thomas <jason@openc3.com>
2023-12-11 20:24:58 -0700

    Fix merge

--- openc3-cosmos-init/plugins/packages/openc3-tool-common/src/components/TargetPacketItemChooser.vue
+++ openc3-cosmos-init/plugins/packages/openc3-tool-common/src/components/TargetPacketItemChooser.vue
@@ -308,11 +308,7 @@ export default {
       }
       this.internalDisabled = true
       const cmd =
-<<<<<<< HEAD
-        this.mode === 'tlm' ? 'get_all_tlm_names' : 'get_all_command_names'
-=======
-        this.mode === 'tlm' ? 'get_all_telemetry_names' : 'get_all_cmd_names'
->>>>>>> main
+        this.mode === 'tlm' ? 'get_all_tlm_names' : 'get_all_cmd_names'
       this.api[cmd](this.selectedTargetName).then((names) => {
         this.packetNames = names.map((name) => {
           return {
@@ -342,11 +338,7 @@ export default {
         return
       }
       this.internalDisabled = true
-<<<<<<< HEAD
-      const cmd = this.mode === 'tlm' ? 'get_tlm' : 'get_command'
-=======
-      const cmd = this.mode === 'tlm' ? 'get_telemetry' : 'get_cmd'
->>>>>>> main
+      const cmd = this.mode === 'tlm' ? 'get_tlm' : 'get_cmd'
       this.api[cmd](this.selectedTargetName, this.selectedPacketName).then(
         (packet) => {
           this.itemNames = packet.items
@@ -427,11 +419,7 @@ export default {
           return value === packet.value
         })
         this.selectedPacketName = packet.value
-<<<<<<< HEAD
-        const cmd = this.mode === 'tlm' ? 'get_tlm' : 'get_command'
-=======
-        const cmd = this.mode === 'tlm' ? 'get_telemetry' : 'get_cmd'
->>>>>>> main
+        const cmd = this.mode === 'tlm' ? 'get_tlm' : 'get_cmd'
         this.api[cmd](this.selectedTargetName, this.selectedPacketName).then(
           (packet) => {
             this.description = packet.description
