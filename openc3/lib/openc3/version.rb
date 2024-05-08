84eee0e6bee0c65999c5255afe648806278aeedb
jmthomas <jmthomas@users.noreply.github.com>
2024-05-08 17:18:45 +0000

    [Github Action] Update version to 5.16.1

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -1,14 +1,14 @@
 # encoding: ascii-8bit
 
-OPENC3_VERSION = '5.16.1-beta0'
+OPENC3_VERSION = '5.16.1'
 module OpenC3
   module Version
     MAJOR = '5'
     MINOR = '16'
     PATCH = '1'
-    OTHER = 'pre.beta0'
-    BUILD = '0150fd4f9eb564e6862bdfaa0ff577543a77daf2'
+    OTHER = ''
+    BUILD = '08a6a56666210ed9741a3f61ce9ec876fe94e1b2'
   end
-  VERSION = '5.16.1-beta0'
-  GEM_VERSION = '5.16.1.pre.beta0'
+  VERSION = '5.16.1'
+  GEM_VERSION = '5.16.1'
 end
