fc8359aeef3cda16bf2beba8e88a8f3310d700c3
jmthomas <jmthomas@users.noreply.github.com>
2024-05-09 03:31:19 +0000

    [Github Action] Update version to 5.16.2-beta0

--- openc3-cosmos-init/plugins/docker-package-install.sh
+++ openc3-cosmos-init/plugins/docker-package-install.sh
@@ -4,7 +4,7 @@ set -e
 PLUGINS="/openc3/plugins"
 GEMS="/openc3/plugins/gems/"
 PACKAGES="packages"
-OPENC3_RELEASE_VERSION=5.16.1
+OPENC3_RELEASE_VERSION=5.16.2-beta0
 
 mkdir -p ${GEMS}
 
--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -1,14 +1,14 @@
 # encoding: ascii-8bit
 
-OPENC3_VERSION = '5.16.1'
+OPENC3_VERSION = '5.16.2-beta0'
 module OpenC3
   module Version
     MAJOR = '5'
     MINOR = '16'
-    PATCH = '1'
-    OTHER = ''
-    BUILD = 'd21b56e140b77e4b5e859c7d51c5544e42a8f833'
+    PATCH = '2'
+    OTHER = 'pre.beta0'
+    BUILD = '751650061f6766d6592bf351e6ec033227050303'
   end
-  VERSION = '5.16.1'
-  GEM_VERSION = '5.16.1'
+  VERSION = '5.16.2-beta0'
+  GEM_VERSION = '5.16.2.pre.beta0'
 end
