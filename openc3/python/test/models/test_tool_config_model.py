db05e90722916c5542b8943162060bb55ce7bceb
Joseph Brothers <joseph@openc3.com>
2024-07-26 18:24:08 -0600

    raise coverage on some smaller Python models

--- openc3/python/test/models/test_tool_config_model.py
+++ openc3/python/test/models/test_tool_config_model.py
@@ -1,4 +1,4 @@
-# Copyright 2023 OpenC3, Inc.
+# Copyright 2023, 2024 OpenC3, Inc.
 # All Rights Reserved.
 #
 # This program is free software; you can modify and/or redistribute it
@@ -20,10 +20,37 @@ from unittest.mock import *
 from test.test_helper import *
 from openc3.models.tool_config_model import ToolConfigModel
 
-
 class TestToolConfigModel(unittest.TestCase):
     def setUp(self):
-        mock_redis(self)
+      mock_redis(self)
+      setup_system()
+
+    #def test_self_load_config(self):
+        #config = ToolConfigModel.load_config('toolie', 'namely', scope= 'DEFAULT')
+        #self.assertEqual(config, {})
+
+    def test_self_save_config(self):
+        #self.assertRaises(ValueError,
+          result = ToolConfigModel.save_config(
+          'toolie',
+          'namely',
+          '{"data":"value", "other":42, "not":null}',
+           local_mode= True,
+           scope= 'DEFAULT')
+        #) # message (/Read-only file system self. dir_s_mkdir - \/plugins/)
+
+    def test_self_list_configs(self):
+        all = ToolConfigModel.list_configs('toolie', scope= 'DEFAULT')
+        self.assertEqual(all, []) # eq('ps')
+
+    def test_self_config_tool_names(self):
+        names = ToolConfigModel.config_tool_names(scope= 'DEFAULT')
+        self.assertEqual(names, []) # eq('ps')
+
+    def test_deletes(self):
+        name = ToolConfigModel.delete_config('toolie', 'namely', local_mode= True, scope= 'DEFAULT')
+
+    # some overlap
 
     def test_save_load_list_delete(self):
         data = {"key": "value", "longer key": "more data"}
