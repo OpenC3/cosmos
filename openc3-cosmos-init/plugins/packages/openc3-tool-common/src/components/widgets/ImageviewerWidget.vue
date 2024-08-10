34a5a2916291d990077736697ea8dd6648229e95
Jason Thomas <jason@openc3.com>
2024-08-10 09:56:53 -0600

    Update copyright

--- openc3-cosmos-init/plugins/packages/openc3-tool-common/src/components/widgets/ImageviewerWidget.vue
+++ openc3-cosmos-init/plugins/packages/openc3-tool-common/src/components/widgets/ImageviewerWidget.vue
@@ -1,5 +1,5 @@
 <!--
-# Copyright 2022 Ball Aerospace & Technologies Corp.
+# Copyright 2024 OpenC3, Inc.
 # All Rights Reserved.
 #
 # This program is free software; you can modify and/or redistribute it
@@ -12,10 +12,6 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 
-# Modified by OpenC3, Inc.
-# All changes Copyright 2022, OpenC3, Inc.
-# All Rights Reserved
-#
 # This file may also be used under the terms of a commercial license
 # if purchased from OpenC3, Inc.
 -->
