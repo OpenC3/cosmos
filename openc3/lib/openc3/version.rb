c38c4f73671404aaf5181c7d4f360a0b766e8141
ryanmelt <ryanmelt@users.noreply.github.com>
2024-08-19 20:35:48 +0000

    [Github Action] Update version to 5.18.0

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -1,14 +1,14 @@
 # encoding: ascii-8bit
 
-OPENC3_VERSION = '5.17.2-beta0'
+OPENC3_VERSION = '5.18.0'
 module OpenC3
   module Version
     MAJOR = '5'
-    MINOR = '17'
-    PATCH = '2'
-    OTHER = 'pre.beta0'
-    BUILD = '30d516ca252a513dc582a87aaabbe1e24bb02654'
+    MINOR = '18'
+    PATCH = '0'
+    OTHER = ''
+    BUILD = '15e1526c2ad19e88a87fef40bece0867e65f91e0'
   end
-  VERSION = '5.17.2-beta0'
-  GEM_VERSION = '5.17.2.pre.beta0'
+  VERSION = '5.18.0'
+  GEM_VERSION = '5.18.0'
 end
