367ffb5a1bc315fac534d617eed6313258b76ff3
Joseph Brothers <joseph@openc3.com>
2024-09-16 10:19:12 -0600

    merge with existing cases where they are more thorough

--- openc3/spec/interfaces/http_client_interface_spec.rb
+++ openc3/spec/interfaces/http_client_interface_spec.rb
@@ -42,6 +42,13 @@ module OpenC3
     end
 
     describe "#initialize" do
+      it "sets all the instance variables" do
+        i = HttpClientInterface.new('localhost', '8080', 'https', '10', '11', '12')
+        expect(i.name).to eql "HttpClientInterface"
+        expect(i.instance_variable_get(:@hostname)).to eql 'localhost'
+        expect(i.instance_variable_get(:@port)).to eql 8080
+      end
+
       it "initializes with default parameters" do
         interface = HttpClientInterface.new('example.com')
         expect(interface.instance_variable_get(:@hostname)).to eq('example.com')
@@ -63,6 +70,17 @@ module OpenC3
     describe "#connection_string" do
       it "returns the correct URL" do
         expect(@interface.connection_string).to eq('https://example.com:8080')
+      end
+
+      it "builds a human readable connection string" do
+        i = HttpClientInterface.new('localhost', '80', 'http', '10', '11', '12')
+        expect(i.connection_string).to eql "http://localhost"
+
+        i = HttpClientInterface.new('machine', '443', 'https', '10', '11', '12')
+        expect(i.connection_string).to eql "https://machine"
+
+        i = HttpClientInterface.new('127.0.0.1', '8080', 'http', '10', '11', '12')
+        expect(i.connection_string).to eql "http://127.0.0.1:8080"
       end
     end
 
