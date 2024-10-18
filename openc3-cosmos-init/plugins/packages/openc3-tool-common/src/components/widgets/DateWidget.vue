813c8237df5eb268dc586685d82e98e40eb5c89b
Jason Thomas <jason@openc3.com>
2024-10-18 13:43:18 -0600

    Add DATE and TIME widgets

---
+++ openc3-cosmos-init/plugins/packages/openc3-tool-common/src/components/widgets/DateWidget.vue
@@ -0,0 +1,60 @@
+<!--
+# Copyright 2024 OpenC3, Inc.
+# All Rights Reserved.
+#
+# This program is free software; you can modify and/or redistribute it
+# under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation; version 3 with
+# attribution addendums as found in the LICENSE.txt
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# This file may also be used under the terms of a commercial license
+# if purchased from OpenC3, Inc.
+-->
+
+<template>
+  <v-text-field
+    v-model="dateValue"
+    :label="label"
+    :style="computedStyle"
+    type="date"
+    hide-details
+  />
+</template>
+
+<script>
+import Widget from './Widget'
+
+export default {
+  mixins: [Widget],
+  data() {
+    return {
+      label: 'Date',
+      dateValue: '',
+    }
+  },
+  created() {
+    // Look through the settings and see if we're a NAMED_WIDGET
+    this.settings.forEach((setting) => {
+      if (setting[0] === 'NAMED_WIDGET') {
+        setting[2].setNamedWidget(setting[1], this)
+      }
+    })
+    if (this.parameters[0]) {
+      this.label = this.parameters[0]
+    }
+  },
+  methods: {
+    text() {
+      return this.dateValue
+    },
+    date() {
+      return this.dateValue
+    },
+  },
+}
+</script>
