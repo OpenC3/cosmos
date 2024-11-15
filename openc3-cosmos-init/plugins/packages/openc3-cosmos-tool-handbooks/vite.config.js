783c2e4482c3376d226a2d8f16f72175e0bcd567
Ryan Pratt <ryan.pratt@openc3.com>
2024-11-14 18:32:07 -0700

    migrate the rest of the tools to vite except admin

---
+++ openc3-cosmos-init/plugins/packages/openc3-cosmos-tool-handbooks/vite.config.js
@@ -0,0 +1,36 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import { vitePluginSingleSpa } from 'vite-plugin-single-spa'
+import vue from '@vitejs/plugin-vue'
+
+const DEFAULT_EXTENSIONS = ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+
+export default defineConfig({
+  base: '/tools/handbooks',
+  build: {
+    outDir: 'tools/handbooks',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        format: 'system',
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    vitePluginSingleSpa({
+      type: 'mife', // micro front-end
+      serverPort: 2923,
+      spaEntryPoints: 'src/main.js',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+    extensions: [...DEFAULT_EXTENSIONS, '.vue'], // not recommended but saves us from having to change every SFC import
+  },
+  optimizeDeps: {
+    entries: [], // https://github.com/vituum/vituum/issues/25#issuecomment-1690080284
+  }
+})
