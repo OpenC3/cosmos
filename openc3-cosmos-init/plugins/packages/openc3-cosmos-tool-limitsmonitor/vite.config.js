7a22ffaca09d78162137afd002086b79c82c0182
Ryan Pratt <ryan.pratt@openc3.com>
2024-11-20 12:09:48 -0700

    Add dev server plugin
    This is a workaround so that `yarn serve` works like how it did in
    COSMOS 5. Eventually, we should try to figure out why the vite dev
    server isn't working how it's supposed to

--- openc3-cosmos-init/plugins/packages/openc3-cosmos-tool-limitsmonitor/vite.config.js
+++ openc3-cosmos-init/plugins/packages/openc3-cosmos-tool-limitsmonitor/vite.config.js
@@ -2,37 +2,41 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { vitePluginSingleSpa } from 'vite-plugin-single-spa'
 import vue from '@vitejs/plugin-vue'
+import { devServerPlugin } from '@openc3/tool-common/viteDevServerPlugin'
 
 const DEFAULT_EXTENSIONS = ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
 
-export default defineConfig({
-  build: {
-    outDir: 'tools/limitsmonitor',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        format: 'systemjs',
+export default defineConfig((options) => {
+  return {
+    build: {
+      outDir: 'tools/limitsmonitor',
+      emptyOutDir: true,
+      rollupOptions: {
+        output: {
+          format: 'systemjs',
+        },
       },
     },
-  },
-  plugins: [
-    vue(),
-    vitePluginSingleSpa({
-      type: 'mife', // micro front-end
-      serverPort: 2912,
-      spaEntryPoints: 'src/main.js',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+    plugins: [
+      vue(),
+      vitePluginSingleSpa({
+        type: 'mife', // micro front-end
+        serverPort: 2912,
+        spaEntryPoints: 'src/main.js',
+      }),
+      devServerPlugin(options),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+      extensions: [...DEFAULT_EXTENSIONS, '.vue'], // not recommended but saves us from having to change every SFC import
     },
-    extensions: [...DEFAULT_EXTENSIONS, '.vue'], // not recommended but saves us from having to change every SFC import
-  },
-  define: {
-    __BASE_URL__: JSON.stringify('/tools/limitsmonitor'),
-  },
-  optimizeDeps: {
-    entries: [], // https://github.com/vituum/vituum/issues/25#issuecomment-1690080284
-  },
+    define: {
+      __BASE_URL__: JSON.stringify('/tools/limitsmonitor'),
+    },
+    optimizeDeps: {
+      entries: [], // https://github.com/vituum/vituum/issues/25#issuecomment-1690080284
+    },
+  }
 })
