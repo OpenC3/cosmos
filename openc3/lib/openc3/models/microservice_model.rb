72d3661f80f676a78387ca8cb6468998feb0c5f3
Jason Thomas <jason@openc3.com>
2024-12-02 16:08:53 -0700

    Revert "Sharded Operator Support"
    This reverts commit e25d5e80db958154d1b85d5e7aa1fdc5192d8472.

--- openc3/lib/openc3/models/microservice_model.rb
+++ openc3/lib/openc3/models/microservice_model.rb
@@ -44,7 +44,6 @@ module OpenC3
     attr_accessor :prefix
     attr_accessor :disable_erb
     attr_accessor :ignore_changes
-    attr_accessor :shard
 
     # NOTE: The following three class methods are used by the ModelController
     # and are reimplemented to enable various Model class methods to work
@@ -106,7 +105,6 @@ module OpenC3
       prefix: nil,
       disable_erb: nil,
       ignore_changes: nil,
-      shard: 0,
       scope:
     )
       parts = name.split("__")
@@ -133,7 +131,6 @@ module OpenC3
       @prefix = prefix
       @disable_erb = disable_erb
       @ignore_changes = ignore_changes
-      @shard = shard.to_i # to_i to handle nil
       @bucket = Bucket.getClient()
     end
 
@@ -156,8 +153,7 @@ module OpenC3
         'secrets' => @secrets.as_json(*a),
         'prefix' => @prefix,
         'disable_erb' => @disable_erb,
-        'ignore_changes' => @ignore_changes,
-        'shard' => @shard,
+        'ignore_changes' => @ignore_changes
       }
     end
 
@@ -219,9 +215,6 @@ module OpenC3
         if parameters
           @disable_erb.concat(parameters)
         end
-      when 'SHARD'
-        parser.verify_num_parameters(1, 1, "#{keyword} <Shard Number Starting from 0>")
-        @shard = Integer(parameters[0])
       else
         raise ConfigParser::Error.new(parser, "Unknown keyword and parameters for Microservice: #{keyword} #{parameters.join(" ")}")
       end
