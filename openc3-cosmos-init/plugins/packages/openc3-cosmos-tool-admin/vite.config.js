bc1ed32722ed62029cf7754556e6dee0d39b7d19
Ryan Pratt <ryan.pratt@openc3.com>
2024-12-05 16:59:05 -0700

    Switch ace-diff to ESM

--- openc3-cosmos-init/plugins/packages/openc3-cosmos-tool-admin/vite.config.js
+++ openc3-cosmos-init/plugins/packages/openc3-cosmos-tool-admin/vite.config.js
@@ -22,9 +22,6 @@ export default defineConfig((options) => {
         external: ['single-spa', 'vue', 'vuex', 'vue-router', 'vuetify'],
         preserveEntrySignatures: 'strict',
       },
-      commonjsOptions: {
-        include: [/ace-diff/, /node_modules/],
-      },
     },
     server: {
       port: 2930,
@@ -50,7 +47,6 @@ export default defineConfig((options) => {
     },
     optimizeDeps: {
       entries: [], // https://github.com/vituum/vituum/issues/25#issuecomment-1690080284
-      include: ['@openc3/ace-diff'],
     },
   }
 })
