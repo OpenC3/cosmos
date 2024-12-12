17e63e051b4aee7bc638fe7ce8a43da5b85cfab4
Ryan Pratt <ryan.pratt@openc3.com>
2024-12-11 17:00:40 -0700

    Partial work migrating angular template
    The CdkPortal thing isn't getting style and the file menu isn't
    visible... this seems unrelated to the COSMOS 6 stuff?

--- openc3/templates/tool_angular/src/app/app.component.ts
+++ openc3/templates/tool_angular/src/app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent implements  AfterViewInit, OnDestroy {
     if (elem) {
       return elem
     } else {
-      return new HTMLElement()
+      return document.createElement('span')
     }
   }
   ngAfterViewInit(): void {
@@ -34,11 +34,12 @@ export class AppComponent implements  AfterViewInit, OnDestroy {
     document.title = "<%= tool_name_display %>";
     let tool = document.querySelector('#openc3-tool');
     if (tool == null) {
-      tool = new HTMLElement()
+      tool = document.createElement('div')
     }
     let overlay = document.querySelector('.cdk-overlay-container');
     if (overlay == null) {
-      overlay = new HTMLElement()
+      overlay = document.createElement('div')
+      overlay.classList.add('cdk-overlay-container');
     }
     // Move stuff
     tool.append(overlay);
