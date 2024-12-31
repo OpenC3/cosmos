5fc21300157eded7a5510193202811ec27280dd1
Ryan Pratt <ryan.pratt@openc3.com>
2024-12-31 11:48:58 -0700

    Add Edge and Safari to browsercheck

--- openc3-cosmos-init/plugins/packages/openc3-tool-base/public/js/browsercheck.js
+++ openc3-cosmos-init/plugins/packages/openc3-tool-base/public/js/browsercheck.js
@@ -21,13 +21,18 @@
 
   const minChromeVersion = 108 // November 2022
   const minFirefoxVersion = 108 // December 2022
-  const currentMinVersionString = `c${minChromeVersion},f${minFirefoxVersion}`
+  const minEdgeVersion = 108 // December 2022
+  const minSafariVersion = 16 // September 2022
+  const currentMinVersionString = `c${minChromeVersion},f${minFirefoxVersion},e${minEdgeVersion},s${minSafariVersion}`
 
   const uaChrome = navigator.userAgent.match(/Chrome\/\d+/)
   const uaFirefox = navigator.userAgent.match(/Firefox\/\d+/)
+  const uaEdge = navigator.userAgent.match(/Edg\/\d+/) // `/Edge\/\d+/` is Legacy Edge (pre-Chromium)
+  const uaSafari = navigator.userAgent.match(/Safari\/\d+/)
 
   // Reset the nag counter when there are updates
-  const isNewMinVersion = localStorage.lastCheckedBrowserVersions !== currentMinVersionString
+  const isNewMinVersion =
+    localStorage.lastCheckedBrowserVersions !== currentMinVersionString
   const isNewUaString = localStorage.lastUaString !== navigator.userAgent
   const isCounterUnset = parseInt(localStorage.browserCheckAlertCounter) === NaN
   if (isNewMinVersion || isNewUaString || isCounterUnset) {
@@ -46,12 +51,38 @@
     return parseInt(ua.at(0).split('/').at(1))
   }
 
-  if (uaChrome) {
+  /* Order matters here for Edge, Chrome, and Safari because their user-agent strings match each other.
+   * Edge: `Chrome/x Safari/x Edg/x`
+   * Chrome: `Chrome/x Safari/x`
+   * Safari: `Version/x Safari/x`
+   * Firefox: `Firefox/x`
+   *
+   * If we need to get any fancier than this, we should look into a parser library
+   * such as https://github.com/faisalman/ua-parser-js
+   */
+  if (uaEdge) {
+    const version = parseUaVersion(uaEdge)
+    if (version === NaN || version < minEdgeVersion) {
+      localStorage.browserCheckAlertCounter++
+      alert(
+        `It looks like you're using an unsupported version of Edge. Please update to version ${minEdgeVersion} or later.`,
+      )
+    }
+  } else if (uaChrome) {
     const version = parseUaVersion(uaChrome)
     if (version === NaN || version < minChromeVersion) {
       localStorage.browserCheckAlertCounter++
       alert(
         `It looks like you're using an unsupported version of Chrome. Please update to version ${minChromeVersion} or later.`,
+      )
+    }
+  } else if (uaSafari) {
+    const uaSafariVersion = navigator.userAgent.match(/Version\/\d+/)
+    const version = parseUaVersion(uaSafariVersion)
+    if (version === NaN || version < minSafariVersion) {
+      localStorage.browserCheckAlertCounter++
+      alert(
+        `It looks like you're using an unsupported version of Safari. Please update to version ${minSafariVersion} or later.`,
       )
     }
   } else if (uaFirefox) {
