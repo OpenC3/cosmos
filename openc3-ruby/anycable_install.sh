0f75b2793341c0731d216d61343e0f4ecf2eea70
Ryan Melton <ryan@openc3.com>
2025-01-25 08:10:27 -0700

    fix anycable-go permissions

--- openc3-ruby/anycable_install.sh
+++ openc3-ruby/anycable_install.sh
@@ -2,7 +2,10 @@
 
 ARCH=`uname -m`
 if [ "$ARCH" == "aarch64" ]; then
+  chmod 755 /usr/bin/anycable-go-linux-arm64
   ln -s /usr/bin/anycable-go-linux-arm64 /usr/bin/anycable-go
 else
+  chmod 755 /usr/bin/anycable-go-linux-amd64
   ln -s /usr/bin/anycable-go-linux-amd64 /usr/bin/anycable-go
 fi
+chmod 755 /usr/bin/anycable-go
