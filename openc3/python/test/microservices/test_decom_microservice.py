06bebb1a4b45dbae93d9c2607939951e196e4ad9
Jason Thomas <jason@openc3.com>
2025-02-18 15:43:25 -0700

    Format and lint

--- openc3/python/test/microservices/test_decom_microservice.py
+++ openc3/python/test/microservices/test_decom_microservice.py
@@ -40,9 +40,10 @@ class TestDecomMicroservice(unittest.TestCase):
         redis = mock_redis(self)
         setup_system()
 
-        limits_set.return_value = 'DEFAULT'
+        limits_set.return_value = "DEFAULT"
 
         orig_xread = redis.xread
+
         # Override xread to ignore the block and count keywords
         def xread_side_effect(*args, **kwargs):
             if "block" in kwargs:
@@ -65,26 +66,31 @@ class TestDecomMicroservice(unittest.TestCase):
         model.create()
         model = TargetModel(name="SYSTEM", scope="DEFAULT")
         model.create()
-        model = MicroserviceModel("DEFAULT__DECOM__INST_INT", scope="DEFAULT", topics=[
+        model = MicroserviceModel(
+            "DEFAULT__DECOM__INST_INT",
+            scope="DEFAULT",
+            topics=[
                 "DEFAULT__TELEMETRY__{INST}__HEALTH_STATUS",
-            ], target_names=["INST"] )
+            ],
+            target_names=["INST"],
+        )
         model.create()
         self.dm = DecomMicroservice("DEFAULT__DECOM__INST_INT")
         self.dm_thread = threading.Thread(target=self.dm.run)
         self.dm_thread.start()
-        time.sleep(0.001) # Allow the thread to start
+        time.sleep(0.001)  # Allow the thread to start
 
     def tearDown(self):
         self.dm.shutdown()
         self.dm_thread.join()
 
     def test_run_decommutates_a_packet_from_raw_to_engineering_values(self):
-        packet = System.telemetry.packet('INST', 'HEALTH_STATUS')
+        packet = System.telemetry.packet("INST", "HEALTH_STATUS")
         packet.extra = {}
-        packet.extra['STATUS'] = 'OK'
+        packet.extra["STATUS"] = "OK"
         packet.received_time = datetime.now(timezone.utc)
         for stdout in capture_io():
-            TelemetryTopic.write_packet(packet, scope='DEFAULT')
+            TelemetryTopic.write_packet(packet, scope="DEFAULT")
             time.sleep(0.01)
             self.assertIn("INST HEALTH_STATUS TEMP1 = -100.0 is RED_LOW (-80.0)", stdout.getvalue())
             self.assertIn("INST HEALTH_STATUS TEMP2 = -100.0 is RED_LOW (-60.0)", stdout.getvalue())
@@ -97,67 +103,67 @@ class TestDecomMicroservice(unittest.TestCase):
         self.assertEqual(tlm("INST HEALTH_STATUS TEMP3"), -100.0)
         self.assertEqual(tlm("INST HEALTH_STATUS TEMP4"), -100.0)
 
-        events = LimitsEventTopic.read(0, scope= "DEFAULT")
+        events = LimitsEventTopic.read(0, scope="DEFAULT")
         self.assertEqual(len(events), 6)
         # Check the first one completely
-        self.assertEqual(events[0][1].get('type'), "LIMITS_CHANGE")
-        self.assertEqual(events[0][1].get('target_name'), "INST")
-        self.assertEqual(events[0][1].get('packet_name'), "HEALTH_STATUS")
-        self.assertEqual(events[0][1].get('item_name'), "TEMP1")
-        self.assertEqual(events[0][1].get('old_limits_state'), "None")
-        self.assertEqual(events[0][1].get('new_limits_state'), "RED_LOW")
-        self.assertEqual(events[0][1].get('message'), "INST HEALTH_STATUS TEMP1 = -100.0 is RED_LOW (-80.0)")
-        self.assertEqual(events[1][1].get('message'), "INST HEALTH_STATUS TEMP2 = -100.0 is RED_LOW (-60.0)")
-        self.assertEqual(events[2][1].get('message'), "INST HEALTH_STATUS TEMP3 = -100.0 is RED_LOW (-25.0)")
-        self.assertEqual(events[3][1].get('message'), "INST HEALTH_STATUS TEMP4 = -100.0 is RED_LOW (-80.0)")
-        self.assertEqual(events[4][1].get('message'), "INST HEALTH_STATUS GROUND1STATUS = UNAVAILABLE is YELLOW")
-        self.assertEqual(events[5][1].get('message'), "INST HEALTH_STATUS GROUND2STATUS = UNAVAILABLE is YELLOW")
+        self.assertEqual(events[0][1].get("type"), "LIMITS_CHANGE")
+        self.assertEqual(events[0][1].get("target_name"), "INST")
+        self.assertEqual(events[0][1].get("packet_name"), "HEALTH_STATUS")
+        self.assertEqual(events[0][1].get("item_name"), "TEMP1")
+        self.assertEqual(events[0][1].get("old_limits_state"), "None")
+        self.assertEqual(events[0][1].get("new_limits_state"), "RED_LOW")
+        self.assertEqual(events[0][1].get("message"), "INST HEALTH_STATUS TEMP1 = -100.0 is RED_LOW (-80.0)")
+        self.assertEqual(events[1][1].get("message"), "INST HEALTH_STATUS TEMP2 = -100.0 is RED_LOW (-60.0)")
+        self.assertEqual(events[2][1].get("message"), "INST HEALTH_STATUS TEMP3 = -100.0 is RED_LOW (-25.0)")
+        self.assertEqual(events[3][1].get("message"), "INST HEALTH_STATUS TEMP4 = -100.0 is RED_LOW (-80.0)")
+        self.assertEqual(events[4][1].get("message"), "INST HEALTH_STATUS GROUND1STATUS = UNAVAILABLE is YELLOW")
+        self.assertEqual(events[5][1].get("message"), "INST HEALTH_STATUS GROUND2STATUS = UNAVAILABLE is YELLOW")
 
         packet.disable_limits("TEMP3")
         packet.write("TEMP1", 0.0)
         packet.write("TEMP2", 0.0)
         packet.write("TEMP3", 0.0)
         for stdout in capture_io():
-            TelemetryTopic.write_packet(packet, scope= 'DEFAULT')
+            TelemetryTopic.write_packet(packet, scope="DEFAULT")
             time.sleep(0.01)
             assert re.search(r"INST HEALTH_STATUS TEMP1 = .* is BLUE \(-20.0 to 20.0\)", stdout.getvalue())
             assert re.search(r"INST HEALTH_STATUS TEMP2 = .* is GREEN \(-55.0 to 30.0\)", stdout.getvalue())
 
         # Start reading from the last event's ID
-        events = LimitsEventTopic.read(events[-1][0], scope= "DEFAULT")
+        events = LimitsEventTopic.read(events[-1][0], scope="DEFAULT")
         self.assertEqual(len(events), 3)
-        self.assertEqual(events[0][1]['type'], "LIMITS_CHANGE")
-        self.assertEqual(events[0][1]['target_name'], "INST")
-        self.assertEqual(events[0][1]['packet_name'], "HEALTH_STATUS")
-        self.assertEqual(events[0][1]['item_name'], "TEMP3")
-        self.assertEqual(events[0][1]['old_limits_state'], "RED_LOW")
-        self.assertEqual(events[0][1]['new_limits_state'], "None")
-        self.assertEqual(events[0][1]['message'], "INST HEALTH_STATUS TEMP3 is disabled")
-        assert re.search(r"INST HEALTH_STATUS TEMP1 = .* is BLUE \(-20.0 to 20.0\)", events[1][1]['message'])
-        assert re.search(r"INST HEALTH_STATUS TEMP2 = .* is GREEN \(-55.0 to 30.0\)", events[2][1]['message'])
+        self.assertEqual(events[0][1]["type"], "LIMITS_CHANGE")
+        self.assertEqual(events[0][1]["target_name"], "INST")
+        self.assertEqual(events[0][1]["packet_name"], "HEALTH_STATUS")
+        self.assertEqual(events[0][1]["item_name"], "TEMP3")
+        self.assertEqual(events[0][1]["old_limits_state"], "RED_LOW")
+        self.assertEqual(events[0][1]["new_limits_state"], "None")
+        self.assertEqual(events[0][1]["message"], "INST HEALTH_STATUS TEMP3 is disabled")
+        assert re.search(r"INST HEALTH_STATUS TEMP1 = .* is BLUE \(-20.0 to 20.0\)", events[1][1]["message"])
+        assert re.search(r"INST HEALTH_STATUS TEMP2 = .* is GREEN \(-55.0 to 30.0\)", events[2][1]["message"])
 
     def test_handles_exceptions_in_the_thread(self):
-        with patch.object(self.dm, 'microservice_cmd') as mock_microservice_cmd:
+        with patch.object(self.dm, "microservice_cmd") as mock_microservice_cmd:
             mock_microservice_cmd.side_effect = Exception("Bad command")
             for stdout in capture_io():
-                Topic.write_topic("MICROSERVICE__DEFAULT__DECOM__INST_INT", { 'connect': 'true' }, '*', 100)
+                Topic.write_topic("MICROSERVICE__DEFAULT__DECOM__INST_INT", {"connect": "true"}, "*", 100)
                 time.sleep(0.01)
                 self.assertIn("Decom error Exception('Bad command')", stdout.getvalue())
             # This is an implementation detail but we want to ensure the error was logged
-            self.assertEqual(self.dm.metric.data['decom_error_total']['value'], 1)
+            self.assertEqual(self.dm.metric.data["decom_error_total"]["value"], 1)
 
     def test_handles_exceptions_in_user_processors(self):
-        packet = System.telemetry.packet('INST', 'HEALTH_STATUS')
+        packet = System.telemetry.packet("INST", "HEALTH_STATUS")
         processor = Processor()
         processor.call = lambda packet, buffer: exec("raise RuntimeError('Bad processor')")
-        packet.processors['TEMP1'] = processor
+        packet.processors["TEMP1"] = processor
         packet.received_time = datetime.now(timezone.utc)
         for stdout in capture_io():
-            TelemetryTopic.write_packet(packet, scope='DEFAULT')
+            TelemetryTopic.write_packet(packet, scope="DEFAULT")
             time.sleep(0.01)
             self.assertIn("Bad processor", stdout.getvalue())
         # This is an implementation detail but we want to ensure the error was logged
-        self.assertEqual(self.dm.metric.data['decom_error_total']['value'], 1)
+        self.assertEqual(self.dm.metric.data["decom_error_total"]["value"], 1)
         # CVT is still set
         self.assertEqual(tlm("INST HEALTH_STATUS TEMP1"), -100.0)
         self.assertEqual(tlm("INST HEALTH_STATUS TEMP2"), -100.0)
@@ -167,30 +173,30 @@ class TestDecomMicroservice(unittest.TestCase):
             def call(packet, item, old_limits_state):
                 time.sleep(0.1)
 
-        packet = System.telemetry.packet('INST', 'HEALTH_STATUS')
+        packet = System.telemetry.packet("INST", "HEALTH_STATUS")
         temp1 = packet.get_item("TEMP1")
         temp1.limits.response = DelayedLimitsResponse()
         packet.received_time = datetime.now(timezone.utc)
         for stdout in capture_io():
-            TelemetryTopic.write_packet(packet, scope='DEFAULT')
+            TelemetryTopic.write_packet(packet, scope="DEFAULT")
             time.sleep(0.01)
 
         # Verify that even though the limits response sleeps for 0.1s, the decom thread is not blocked
-        self.assertLess(self.dm.metric.data['decom_duration_seconds']['value'], 0.01)
+        self.assertLess(self.dm.metric.data["decom_duration_seconds"]["value"], 0.01)
 
     def test_handles_exceptions_in_limits_responses(self):
         class BadLimitsResponse(LimitsResponse):
             def call(self, packet, item, old_limits_state):
                 raise Exception("Bad response")
 
-        packet = System.telemetry.packet('INST', 'HEALTH_STATUS')
+        packet = System.telemetry.packet("INST", "HEALTH_STATUS")
         temp1 = packet.get_item("TEMP1")
         temp1.limits.response = BadLimitsResponse()
         packet.received_time = datetime.now(timezone.utc)
         for stdout in capture_io():
-            TelemetryTopic.write_packet(packet, scope='DEFAULT')
+            TelemetryTopic.write_packet(packet, scope="DEFAULT")
             time.sleep(0.01)
             self.assertIn("INST HEALTH_STATUS TEMP1 Limits Response Exception!", stdout.getvalue())
             self.assertIn("Bad response", stdout.getvalue())
 
-        self.assertEqual(self.dm.limits_response_thread.metric.data['limits_response_error_total']['value'], 1)
+        self.assertEqual(self.dm.limits_response_thread.metric.data["limits_response_error_total"]["value"], 1)
