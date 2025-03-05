2e99692d8bb46c8b75914ab53902fc487c7bd1f2
Jason Thomas <jason@openc3.com>
2025-03-04 18:41:13 -0700

    Improve logic

--- openc3-cosmos-init/plugins/packages/openc3-cosmos-tool-packetviewer/src/tools/PacketViewer/PacketViewer.vue
+++ openc3-cosmos-init/plugins/packages/openc3-cosmos-tool-packetviewer/src/tools/PacketViewer/PacketViewer.vue
@@ -492,12 +492,10 @@ export default {
     filter(value, search, _item) {
       if (this.isPinned(value)) {
         return true
+      } else if (value) {
+        return value.toString().indexOf(search.toUpperCase()) >= 0
       } else {
-        if (value) {
-          return value.toString().indexOf(search.toUpperCase()) >= 0
-        } else {
-          return false
-        }
+        return false
       }
     },
     packetChanged(event) {
