c7b8e37a9a2af83f6897b6aa6ec3c699eb62f90e
Ryan Pratt <ryan.pratt@openc3.com>
2025-04-14 16:07:58 -0600

    fix eslint configs

--- openc3-cosmos-init/plugins/packages/openc3-js-common/eslint.config.mjs
+++ openc3-cosmos-init/plugins/packages/openc3-js-common/eslint.config.mjs
@@ -1,25 +1,8 @@
-import prettier from "eslint-plugin-prettier";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-});
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
+import globals from "globals"
 
 export default [
-  ...compat.extends("plugin:prettier/recommended"),
   {
-    plugins: {
-      prettier,
-    },
-
     languageOptions: {
       globals: {
         ...globals.node,
@@ -38,4 +21,5 @@ export default [
       }],
     },
   },
+  eslintPluginPrettierRecommended,
 ];
