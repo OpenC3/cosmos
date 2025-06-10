7f6d6613f041f4f0c8184aa8aefbad173906b204
Clay Kramp <krampcg23@gmail.com>
2025-06-10 15:50:34 -0600

    Try removing ace-diff from CodeCov reporting

--- openc3-cosmos-init/plugins/packages/openc3-cosmos-tool-admin/vite.config.js
+++ openc3-cosmos-init/plugins/packages/openc3-cosmos-tool-admin/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig((options) => {
           chunkFileNames: '[name]-[hash:20].js',
           assetFileNames: 'assets/[name]-[hash][extname]',
         },
-        external: ['single-spa', 'vue', 'vuex', 'vue-router', 'vuetify', '@openc3/ace-diff'],
+        external: ['single-spa', 'vue', 'vuex', 'vue-router', 'vuetify'],
         preserveEntrySignatures: 'strict',
       },
     },
