1c6f7296910a8a81b76fbb2f695964da61dcaf5a
Ryan Pratt <ryan.pratt@openc3.com>
2025-06-20 12:13:20 -0600

    clean up plugin list

--- openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/admin/tabs/plugins/PluginListItem.vue
+++ openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/admin/tabs/plugins/PluginListItem.vue
@@ -22,10 +22,7 @@
       <v-img v-if="image" :src="image" max-height="56" min-width="56" />
     </td>
     <td>
-      <div
-        v-text="title"
-        class="text-h6"
-      />
+      <!-- <div class="text-h6" v-text="name" /> -->
       <div class="text-subtitle-2">
         <!-- subtitle -->
         <template v-if="isModified"> * </template>
@@ -33,57 +30,47 @@
       </div>
       <div v-if="targets?.length">
         <!-- sub-subtitle -->
-        <span
-          v-for="(target, index) in targets"
-          :key="index"
-          class="mr-2"
-        >
+        <span v-for="(target, index) in targets" :key="index" class="mr-2">
           <a
             v-if="target.modified"
-            v-text="target.name"
-            @click.prevent="downloadTarget(target.name)"
             style="cursor: pointer"
-          />
-          <span
-            v-else
+            @click.prevent="downloadTarget(target.name)"
             v-text="target.name"
           />
+          <span v-else v-text="target.name" />
         </span>
       </div>
     </td>
 
     <td>
       <v-menu>
-        <template v-slot:activator="{ props }">
-          <v-icon
-            v-bind="props"
-            icon="mdi-dots-horizontal"
-          />
+        <template #activator="{ props }">
+          <v-icon v-bind="props" icon="mdi-dots-horizontal" />
         </template>
         <v-list>
           <v-list-item
             title="Download"
             prepend-icon="mdi-download"
-            @click="downloadPlugin"
             data-test="download-plugin"
+            @click="downloadPlugin"
           />
           <v-list-item
             title="Edit Details"
             prepend-icon="mdi-pencil"
-            @click="edit"
             data-test="edit-plugin"
+            @click="edit"
           />
           <v-list-item
             title="Upgrade"
             prepend-icon="mdi-update"
-            @click="upgrade"
             data-test="upgrade-plugin"
+            @click="upgrade"
           />
           <v-list-item
             title="Delete"
             prepend-icon="mdi-delete"
-            @click="deletePrompt"
             data-test="delete-plugin"
+            @click="deletePrompt"
           />
         </v-list>
       </v-menu>
@@ -94,19 +81,28 @@
 
 <script>
 import { Api } from '@openc3/js-common/services'
-import { PluginCard, PluginDetailsDialog, PluginProps } from '@/plugins/plugin-store'
+import {
+  // PluginCard,
+  PluginDetailsDialog,
+  PluginProps,
+} from '@/plugins/plugin-store'
 
 export default {
-  mixins: [PluginProps],
   components: {
-    PluginCard,
+    // PluginCard,
     PluginDetailsDialog,
   },
-  emits: ['edit', 'upgrade', 'showDetails'],
+  mixins: [PluginProps],
   props: {
-    targets: Array,
+    targets: {
+      type: Array,
+      default: () => {
+        return []
+      },
+    },
     isModified: Boolean,
   },
+  emits: ['edit', 'delete', 'upgrade', 'showDetails'],
   data() {
     return {
       showCard: false,
@@ -117,21 +113,23 @@ export default {
       this.showCard = true
     },
     downloadPlugin: function () {
-      Api.post(`/openc3-api/packages/${this.name}/download`).then((response) => {
-        // Decode Base64 string
-        const decodedData = window.atob(response.data.contents)
-        // Create UNIT8ARRAY of size same as row data length
-        const uInt8Array = new Uint8Array(decodedData.length)
-        // Insert all character code into uInt8Array
-        for (let i = 0; i < decodedData.length; ++i) {
-          uInt8Array[i] = decodedData.charCodeAt(i)
-        }
-        const blob = new Blob([uInt8Array], { type: 'application/zip' })
-        const link = document.createElement('a')
-        link.href = URL.createObjectURL(blob)
-        link.setAttribute('download', response.data.filename)
-        link.click()
-      })
+      Api.post(`/openc3-api/packages/${this.name}/download`).then(
+        (response) => {
+          // Decode Base64 string
+          const decodedData = window.atob(response.data.contents)
+          // Create UNIT8ARRAY of size same as row data length
+          const uInt8Array = new Uint8Array(decodedData.length)
+          // Insert all character code into uInt8Array
+          for (let i = 0; i < decodedData.length; ++i) {
+            uInt8Array[i] = decodedData.charCodeAt(i)
+          }
+          const blob = new Blob([uInt8Array], { type: 'application/zip' })
+          const link = document.createElement('a')
+          link.href = URL.createObjectURL(blob)
+          link.setAttribute('download', response.data.filename)
+          link.click()
+        },
+      )
     },
     downloadTarget: function (name) {
       Api.post(`/openc3-api/targets/${name}/download`).then((response) => {
