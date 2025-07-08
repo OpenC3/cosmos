c8a4be0269deb7c9389c2e910c4226598e43aaab
jmthomas <176403+jmthomas@users.noreply.github.com>
2025-07-08 18:37:57 +0000

    [Github Action] Update version to 6.6.1-beta0

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -1,14 +1,14 @@
 # encoding: ascii-8bit
 
-OPENC3_VERSION = '6.6.0'
+OPENC3_VERSION = '6.6.1-beta0'
 module OpenC3
   module Version
     MAJOR = '6'
     MINOR = '6'
-    PATCH = '0'
-    OTHER = ''
-    BUILD = 'ceee2aabacbdfe57fe8b5de3580358db2ae3a19a'
+    PATCH = '1'
+    OTHER = 'pre.beta0'
+    BUILD = '88c6c31f501e31bca267ff650232c9c32ce28c2a'
   end
-  VERSION = '6.6.0'
-  GEM_VERSION = '6.6.0'
+  VERSION = '6.6.1-beta0'
+  GEM_VERSION = '6.6.1.pre.beta0'
 end
--- openc3/openc3.gemspec
+++ openc3/openc3.gemspec
@@ -45,7 +45,7 @@ spec = Gem::Specification.new do |s|
     s.platform = Gem::Platform::CURRENT
   end
 
-  s.version = '6.6.0'
+  s.version = '6.6.1.pre.beta0'
   s.licenses = ['AGPL-3.0-only', 'Nonstandard']
 
   # Executables
