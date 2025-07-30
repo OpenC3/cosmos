ed4d52b1433dc2d9a213a7a4461a3d326db56231
Jason Thomas <jason@openc3.com>
2025-07-30 10:07:24 -0600

    Update MINIO/MC

--- scripts/linux/openc3_build_ubi.sh
+++ scripts/linux/openc3_build_ubi.sh
@@ -70,7 +70,7 @@ cd openc3-minio
 docker build \
   --network host \
   --build-arg OPENC3_DEPENDENCY_REGISTRY=${OPENC3_UBI_REGISTRY}/ironbank/opensource \
-  --build-arg OPENC3_MINIO_RELEASE=RELEASE.2025-06-13T11-33-47Z \
+  --build-arg OPENC3_MINIO_RELEASE=RELEASE.2025-07-23T15-54-02Z \
   --platform linux/amd64 \
   -t "${OPENC3_REGISTRY}/${OPENC3_NAMESPACE}/openc3-minio-ubi:${OPENC3_TAG}" \
   .
@@ -156,7 +156,7 @@ docker build \
   --build-context docs=../docs.openc3.com \
   --build-arg NPM_URL=$NPM_URL \
   --build-arg OPENC3_DEPENDENCY_REGISTRY=${OPENC3_UBI_REGISTRY}/ironbank/opensource \
-  --build-arg OPENC3_MC_RELEASE=RELEASE.2025-05-21T01-59-54Z \
+  --build-arg OPENC3_MC_RELEASE=RELEASE.2025-07-21T05-28-08Z \
   --build-arg OPENC3_BASE_IMAGE=openc3-base-ubi \
   --build-arg OPENC3_NODE_IMAGE=openc3-node-ubi \
   --build-arg OPENC3_REGISTRY=$OPENC3_REGISTRY \
