75e3448c0363b457642080e1f9543b6ecca11f03
jmthomas <176403+jmthomas@users.noreply.github.com>
2025-08-05 01:23:23 +0000

    [Github Action] Update version to 6.7.0

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -7,7 +7,7 @@ module OpenC3
     MINOR = '7'
     PATCH = '0'
     OTHER = ''
-    BUILD = '07183bff52168d263202497d8b6b6ccc2b6e05b0'
+    BUILD = '6c4726a21dd004109cf840f83a99f5a8614cfbe3'
   end
   VERSION = '6.7.0'
   GEM_VERSION = '6.7.0'
