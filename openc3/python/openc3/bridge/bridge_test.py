dfa205abba15a5d41178d8737ac3ca60e7c00aae
Clay Kramp <krampcg23@gmail.com>
2025-08-21 12:34:09 -0600

    Cleanup

--- openc3/python/openc3/bridge/bridge_test.py
+++
@@ -1,8 +0,0 @@
-from openc3.bridge.bridge import Bridge
-
-def run_bridge(filename):
-    variables = {}
-    bridge = Bridge(filename, variables)
-    bridge.wait_forever()
-
-run_bridge('bridge.txt')
