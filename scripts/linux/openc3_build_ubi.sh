6270aa70fa6a489e32e14898ecc1faeffec17fc6
Clay Kramp <krampcg23@gmail.com>
2025-09-11 13:19:35 -0600

    Upgrade Traefik to 3.5.0

--- scripts/linux/openc3_build_ubi.sh
+++ scripts/linux/openc3_build_ubi.sh
@@ -140,7 +140,7 @@ docker build \
   --network host \
   --build-arg OPENC3_DEPENDENCY_REGISTRY=${OPENC3_UBI_REGISTRY}/ironbank/opensource/traefik \
   --build-arg TRAEFIK_CONFIG=$TRAEFIK_CONFIG \
-  --build-arg OPENC3_TRAEFIK_RELEASE=v3.5.2 \
+  --build-arg OPENC3_TRAEFIK_RELEASE=v3.5.0 \
   --platform linux/amd64 \
   -t "${OPENC3_REGISTRY}/${OPENC3_NAMESPACE}/openc3-traefik-ubi:${OPENC3_TAG}" \
   .
