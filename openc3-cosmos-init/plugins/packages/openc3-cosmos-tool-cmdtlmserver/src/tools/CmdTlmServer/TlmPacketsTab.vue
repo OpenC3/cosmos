f7656556c922e8fb87af75268c022e8a807893ef
Jason Thomas <jason@openc3.com>
2025-09-13 10:01:15 -0600

    Bump z-index of cmdtlmserver top panel

--- openc3-cosmos-init/plugins/packages/openc3-cosmos-tool-cmdtlmserver/src/tools/CmdTlmServer/TlmPacketsTab.vue
+++ openc3-cosmos-init/plugins/packages/openc3-cosmos-tool-cmdtlmserver/src/tools/CmdTlmServer/TlmPacketsTab.vue
@@ -13,7 +13,7 @@
 # GNU Affero General Public License for more details.
 
 # Modified by OpenC3, Inc.
-# All changes Copyright 2023, OpenC3, Inc.
+# All changes Copyright 2025, OpenC3, Inc.
 # All Rights Reserved
 #
 # This file may also be used under the terms of a commercial license
@@ -84,7 +84,7 @@
       :target-name="d.target_name"
       :packet-name="d.packet_name"
       :visible="true"
-      :z-index="d.zIndex * 2"
+      :z-index="d.zIndex"
       @close="closeRawDialog(d)"
       @focus="focus(d)"
     />
