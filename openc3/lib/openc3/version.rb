ef0697551ca07e292cf329dae31a576dbcf828d2
jmthomas <176403+jmthomas@users.noreply.github.com>
2025-09-13 16:54:37 +0000

    [Github Action] Update version to 6.8.2-beta0

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -1,14 +1,14 @@
 # encoding: ascii-8bit
 
-OPENC3_VERSION = '6.8.1'
+OPENC3_VERSION = '6.8.2-beta0'
 module OpenC3
   module Version
     MAJOR = '6'
     MINOR = '8'
-    PATCH = '1'
-    OTHER = ''
-    BUILD = 'cd2a90541ad224d1dd22e28b4f6a9c0630db26ba'
+    PATCH = '2'
+    OTHER = 'pre.beta0'
+    BUILD = '9ede3c89ae42b2e66b3392af7acb120a278d18cc'
   end
-  VERSION = '6.8.1'
-  GEM_VERSION = '6.8.1'
+  VERSION = '6.8.2-beta0'
+  GEM_VERSION = '6.8.2.pre.beta0'
 end
