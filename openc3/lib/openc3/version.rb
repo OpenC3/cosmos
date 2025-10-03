b0eddcdf24e72a88d5bfde237aa4184fba97a6e3
jmthomas <176403+jmthomas@users.noreply.github.com>
2025-10-03 22:00:06 +0000

    [Github Action] Update version to 6.9.1-beta0

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -1,14 +1,14 @@
 # encoding: ascii-8bit
 
-OPENC3_VERSION = '6.9.0'
+OPENC3_VERSION = '6.9.1-beta0'
 module OpenC3
   module Version
     MAJOR = '6'
     MINOR = '9'
-    PATCH = '0'
-    OTHER = ''
-    BUILD = '92d65ed951bb86b6b155f7e7b7327e889f58e314'
+    PATCH = '1'
+    OTHER = 'pre.beta0'
+    BUILD = '0227123c3368ce9b730d0e41fc378edede1447a1'
   end
-  VERSION = '6.9.0'
-  GEM_VERSION = '6.9.0'
+  VERSION = '6.9.1-beta0'
+  GEM_VERSION = '6.9.1.pre.beta0'
 end
