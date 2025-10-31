0d147bce3bab310ca18c33e7ee086cb68f1c53df
Ryan Pratt <ryan.pratt@openc3.com>
2025-10-31 13:52:40 -0600

    add minimum cosmos version for plugins

--- openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/admin/tabs/PluginsTab.vue
+++ openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/admin/tabs/PluginsTab.vue
@@ -141,6 +141,7 @@
       :plugin-txt="pluginTxt"
       :existing-plugin-txt="existingPluginTxt"
       :store-id="storeId"
+      :min-cosmos-version="minCosmosVersion"
       @callback="pluginCallback"
     />
     <modified-plugin-dialog
@@ -188,6 +189,7 @@ export default {
       pluginName: null,
       variables: {},
       storeId: null,
+      minCosmosVersion: null,
       pluginTxt: '',
       file: null,
       currentPlugin: null,
@@ -271,6 +273,7 @@ export default {
       this.pluginName = null
       this.variables = {}
       this.storeId = null
+      this.minCosmosVersion = null
       this.pluginTxt = ''
       this.file = null
       this.currentPlugin = null
@@ -367,6 +370,7 @@ export default {
           this.pluginName = response.data.name
           this.variables = response.data.variables
           this.storeId = response.data.store_id
+          this.minCosmosVersion = response.data.minimum_cosmos_version
           this.pluginTxt = pluginTxt
           this.existingPluginTxt = existingPluginTxt
           this.showPluginDialog = true
