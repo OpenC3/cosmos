84cda2971e33cbf843d4e1dc2644fcfd32ed8437
ryan-pratt <44932063+ryan-pratt@users.noreply.github.com>
2025-11-05 19:21:03 +0000

    [Github Action] Update version to 6.9.2

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -1,14 +1,14 @@
 # encoding: ascii-8bit
 
-OPENC3_VERSION = '6.9.2-beta0'
+OPENC3_VERSION = '6.9.2'
 module OpenC3
   module Version
     MAJOR = '6'
     MINOR = '9'
     PATCH = '2'
-    OTHER = 'pre.beta0'
-    BUILD = '93824da6dfbfd898ba461e6120af5425080be1f1'
+    OTHER = ''
+    BUILD = '48443021570f0b090c83eb533e149f46072c4bb7'
   end
-  VERSION = '6.9.2-beta0'
-  GEM_VERSION = '6.9.2.pre.beta0'
+  VERSION = '6.9.2'
+  GEM_VERSION = '6.9.2'
 end
