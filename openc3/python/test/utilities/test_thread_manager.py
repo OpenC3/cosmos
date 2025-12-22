4ba858d14d6e49543c1a8493a7a6623936317091
Jason Thomas <jason@openc3.com>
2025-12-22 16:56:26 -0700

    Fix python tests

--- openc3/python/test/utilities/test_thread_manager.py
+++ openc3/python/test/utilities/test_thread_manager.py
@@ -87,23 +87,22 @@ class TestThreadManager(unittest.TestCase):
         def task(duration):
             time.sleep(duration)
 
-        thread1 = threading.Thread(target=task, args=[0.01])
-        thread2 = threading.Thread(target=task, args=[0.1])
-        thread3 = threading.Thread(target=task, args=[0.05])
+        # Use longer durations to reduce timing sensitivity
+        thread1 = threading.Thread(target=task, args=[0.05])
+        thread2 = threading.Thread(target=task, args=[0.2])
+        thread3 = threading.Thread(target=task, args=[0.1])
         ThreadManager.instance().register(thread1)
         ThreadManager.instance().register(thread2)
         ThreadManager.instance().register(thread3)
+        start_time = perf_counter()
         thread1.start()
         thread2.start()
         thread3.start()
-        self.assertTrue(thread1.is_alive())
-        self.assertTrue(thread2.is_alive())
-        self.assertTrue(thread3.is_alive())
-        start_time = perf_counter()
         ThreadManager.instance().join()
         stop_time = perf_counter()
-        # The join should wait for the longest running thread (0.1 seconds)
-        self.assertAlmostEqual(stop_time - start_time, 0.1, delta=0.01)
+        # The join should wait for the longest running thread (0.2 seconds)
+        # Use a larger delta to account for thread scheduling variance
+        self.assertAlmostEqual(stop_time - start_time, 0.2, delta=0.05)
         # All threads should be stopped
         self.assertFalse(thread1.is_alive())
         self.assertFalse(thread2.is_alive())
