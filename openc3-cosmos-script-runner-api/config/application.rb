aa95af68ecfed1e664dbbe7c73929496aae3895a
Jason Thomas <jason@openc3.com>
2025-12-23 10:56:59 -0700

    Upgrade to Rails 8

--- openc3-cosmos-script-runner-api/config/application.rb
+++ openc3-cosmos-script-runner-api/config/application.rb
@@ -24,7 +24,7 @@ Prometheus::Client.config.data_store = Prometheus::Client::DataStores::DirectFil
 module ScriptRunnerApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 8.0
     # Add to hosts to prevent "Blocked host" errors
     config.hosts.clear
 
