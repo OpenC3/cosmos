24da66fa0798c5609bbb26c91279bc69811cb4d6
Emily Ragan <emily@openc3.com>
2025-12-30 14:53:46 -0700

    Update example with lessons learned

--- openc3/python/examples/cosmos_web_socket_example.py
+++ openc3/python/examples/cosmos_web_socket_example.py
@@ -54,6 +54,7 @@ data = StreamingWebSocketApi.read_all(
 print(data)
 
 
+import time
 now = datetime.now(timezone.utc)
 with MessagesWebSocketApi(
     history_count=0,
@@ -61,9 +62,10 @@ with MessagesWebSocketApi(
     end_time=to_nsec_from_epoch(now - timedelta(seconds=60)),
 ) as api:
     for _ in range(500):
-        # Note returns batch array
-        data = api.read()
+        # Note returns batch array, default timeout is None, which will wait indefinitely until a new message is available
+        data = api.read(timeout=1)
         if not data or len(data) == 0:
             break
         print(f"\nReceived {len(data)} log messages:")
         print(data)
+        time.sleep(0.1) # Allow for a brief pause between reads to allow PING/PONG communication in order to keep connection alive
