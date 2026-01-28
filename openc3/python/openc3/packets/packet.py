39b4cd13e9928e55f812c61a543a5ede045c331a
Jason Thomas <jason@openc3.com>
2026-01-28 09:55:39 -0500

    Update reserved items

--- openc3/python/openc3/packets/packet.py
+++ openc3/python/openc3/packets/packet.py
@@ -53,8 +53,7 @@ class Packet(Structure):
         "RECEIVED_TIMESECONDS",
         "RECEIVED_TIMEFORMATTED",
         "RECEIVED_COUNT",
-        "TIMESTAMP",  # Reserved for QuestDB time series database
-        "RX_TIMESTAMP",  # Reserved for QuestDB received time column
+        "COSMOS_DATA_TAG",  # Reserved for QuestDB time series database
     }
     ANY_STATE = "ANY"
     # Valid format types
