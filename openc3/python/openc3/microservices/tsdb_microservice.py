9ab231707142df9d66b6ea5ae20cbe92dc8a8877
Ryan Melton <ryan@openc3.com>
2026-02-15 11:48:49 -0700

    fix ruff

--- openc3/python/openc3/microservices/tsdb_microservice.py
+++ openc3/python/openc3/microservices/tsdb_microservice.py
@@ -23,8 +23,9 @@ from openc3.microservices.microservice import Microservice
 from openc3.topics.config_topic import ConfigTopic
 from openc3.topics.topic import Topic
 from openc3.utilities.questdb_client import QuestDBClient
-from openc3.utilities.thread_manager import ThreadManager
 from openc3.utilities.store import EphemeralStore
+from openc3.utilities.thread_manager import ThreadManager
+
 
 class TsdbMicroservice(Microservice):
     TRIM_KEEP_MS = 60000 # 1 minute
