64b376bcab48c6854ec535e0e76e69637571581a
ryanmelt <184686+ryanmelt@users.noreply.github.com>
2026-02-19 23:28:02 +0000

    [Github Action] Update version to 7.0.0-rc2

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -7,7 +7,7 @@ module OpenC3
     MINOR = '0'
     PATCH = '0'
     OTHER = 'pre.rc2'
-    BUILD = '132648bad3b202bc5acf4d099d0af2341ec97047'
+    BUILD = 'f3aa0d40478504753a1f4728bae52e02342c50f9'
   end
   VERSION = '7.0.0-rc2'
   GEM_VERSION = '7.0.0.pre.rc2'
