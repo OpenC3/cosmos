d815d320892757d85ddcea88e5d8784b4a4bf1d6
Ryan Melton <ryan@openc3.com>
2026-03-12 13:02:03 -0600

    Increase ulimit for TSDB. Update depup to just packet time and tag

--- openc3/python/openc3/utilities/questdb_client.py
+++ openc3/python/openc3/utilities/questdb_client.py
@@ -699,13 +699,13 @@ class QuestDBClient:
                     if columns_sql:
                         sql += f",\n{columns_sql}"
 
-                    # Primary DEDUP will be on PACKET_TIMESECONDS, RECEIVED_TIMESECONDS
-                    # If for some reason you're duplicating RECEIVED_TIMESECONDS you can
+                    # Primary DEDUP will be on PACKET_TIMESECONDS
+                    # If for some reason you're duplicating PACKET_TIMESECONDS you can
                     # explicitly include COSMOS_DATA_TAG as well.
                     sql += """
                         ) TIMESTAMP(PACKET_TIMESECONDS)
                             PARTITION BY DAY
-                            DEDUP UPSERT KEYS (PACKET_TIMESECONDS, RECEIVED_TIMESECONDS, COSMOS_DATA_TAG)
+                            DEDUP UPSERT KEYS (PACKET_TIMESECONDS, COSMOS_DATA_TAG)
                     """
 
                     # Add TTL clause if specified
