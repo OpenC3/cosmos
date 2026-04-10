88d58d6c3529c117fafbb726c8fe970b9f3e48ca
jmthomas <176403+jmthomas@users.noreply.github.com>
2026-04-10 22:04:45 +0000

    [Github Action] Update version to 7.0.1

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -1,14 +1,14 @@
 # encoding: ascii-8bit
 
-OPENC3_VERSION = '7.0.1-beta0'
+OPENC3_VERSION = '7.0.1'
 module OpenC3
   module Version
     MAJOR = '7'
     MINOR = '0'
     PATCH = '1'
-    OTHER = 'pre.beta0'
-    BUILD = 'eb3e364e8f3d5a7ea68fd756625561192d0025f0'
+    OTHER = ''
+    BUILD = '665de79cef884b8d470b08817fd4a1fb538fb187'
   end
-  VERSION = '7.0.1-beta0'
-  GEM_VERSION = '7.0.1.pre.beta0'
+  VERSION = '7.0.1'
+  GEM_VERSION = '7.0.1'
 end
