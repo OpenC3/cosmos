09ca09d7ce6418d6099e0b35fd830fc24d0b1dfe
Emily Ragan <emily@openc3.com>
2026-04-13 13:07:34 -0600

    Add/update copyright header

--- openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/scriptrunner/useScriptPrompts.js
+++ openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/scriptrunner/useScriptPrompts.js
@@ -1,12 +1,12 @@
-/*
-# Copyright 2026, OpenC3, Inc.
+/**
+# Copyright 2026 OpenC3, Inc.
 # All Rights Reserved.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE.md for more details.
-
+#
 # This file may also be used under the terms of a commercial license
 # if purchased from OpenC3, Inc.
 */
--- openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/scriptrunner/utils.js
+++ openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/scriptrunner/utils.js
@@ -1,3 +1,16 @@
+/**
+# Copyright 2026 OpenC3, Inc.
+# All Rights Reserved.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE.md for more details.
+#
+# This file may also be used under the terms of a commercial license
+# if purchased from OpenC3, Inc.
+*/
+
 export function detectLanguage(text) {
   let rubyRegex1 = /^\s*(require|load|puts) /
   let pythonRegex1 = /^\s*(import|from) /
