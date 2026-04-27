3e0159eac213b1b85b56fcbcf2f870f276ad78ed
Jason Thomas <jason@openc3.com>
2026-04-26 21:35:21 -0600

    Format calendar activity with correct timezone

--- openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/calendar/CreateDialog.js
+++ openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/calendar/CreateDialog.js
@@ -1,5 +1,5 @@
 /*
-# Copyright 2023 OpenC3, Inc.
+# Copyright 2026 OpenC3, Inc.
 # All Rights Reserved.
 #
 # This program is distributed in the hope that it will be useful,
@@ -39,21 +39,28 @@ export default {
         this.startDate = this.date
         this.endDate = this.date
       } else {
-        this.startDate = format(new Date(), 'yyyy-MM-dd')
-        this.endDate = format(new Date(), 'yyyy-MM-dd')
+        this.startDate = this.formatDate(new Date(), this.timeZone)
+        this.endDate = this.startDate
       }
       let ms = 1000 * 60 * 30 // 30 min
       let start = null
       if (this.time) {
+        // this.time is already in the display timezone; parse/format roundtrip
+        // in local time preserves the HH:mm:ss the user clicked on.
         start = parse(this.time, 'HH:mm:ss', new Date())
         // Round down so the start is the beginning of the time box where they clicked
         start = new Date(Math.floor(start.getTime() / ms) * ms)
+        this.startTime = format(start, 'HH:mm:ss')
+        this.endTime = format(add(start, { minutes: 30 }), 'HH:mm:ss')
       } else {
         // Round up so the start is in the future
         start = new Date(Math.ceil(new Date().getTime() / ms) * ms)
+        this.startTime = this.formatTimeHMS(start, this.timeZone)
+        this.endTime = this.formatTimeHMS(
+          add(start, { minutes: 30 }),
+          this.timeZone,
+        )
       }
-      this.startTime = format(start, 'HH:mm:ss')
-      this.endTime = format(add(start, { minutes: 30 }), 'HH:mm:ss')
     },
   },
 }
