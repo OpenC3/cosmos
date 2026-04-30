3ff50689c39121231e66845a071115bd3824484d
Jason Thomas <jason@openc3.com>
2026-04-30 15:29:48 -0600

    Fix sleeper.py to use sockets not pipes

--- openc3/python/openc3/utilities/sleeper.py
+++ openc3/python/openc3/utilities/sleeper.py
@@ -10,15 +10,18 @@
 # if purchased from OpenC3, Inc.
 
 import contextlib
-import os
 import selectors
+import socket
 
 
 # Allows for a breakable sleep implementation using the self-pipe trick
 # See http://www.sitepoint.com/the-self-pipe-trick-explained/
+# socket.socketpair (rather than os.pipe) so the readable end can be
+# registered with selectors on Windows, where the default selector is
+# backed by select.select which only accepts sockets.
 class Sleeper:
     def __init__(self):
-        self.pipe_reader, self.pipe_writer = os.pipe()
+        self.pipe_reader, self.pipe_writer = socket.socketpair()
         self.selector = selectors.DefaultSelector()
         self.selector.register(self.pipe_reader, selectors.EVENT_READ)
         self.canceled = False
@@ -31,7 +34,7 @@ class Sleeper:
         list = self.selector.select(timeout=seconds)
         if list and list[0]:
             with contextlib.suppress(Exception):
-                os.close(self.pipe_reader)
+                self.pipe_reader.close()
             return True
         else:
             return False
@@ -40,5 +43,6 @@ class Sleeper:
     def cancel(self):
         if not self.canceled:
             self.canceled = True
-            os.write(self.pipe_writer, bytes(".", encoding="ascii"))
-            os.close(self.pipe_writer)
+            with contextlib.suppress(OSError):
+                self.pipe_writer.send(b".")
+            self.pipe_writer.close()
