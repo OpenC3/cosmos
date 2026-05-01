53db6482ced349c94c15e2fb93cb70fa3d7b9d12
Jason Thomas <jason@openc3.com>
2026-04-30 18:36:28 -0600

    Complete release

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -1,14 +1,14 @@
 # encoding: ascii-8bit
 
-OPENC3_VERSION = '7.1.0'
+OPENC3_VERSION = '7.1.1-beta0'
 module OpenC3
   module Version
     MAJOR = '7'
     MINOR = '1'
-    PATCH = '0'
-    OTHER = ''
-    BUILD = '1074049d7a87d4b4d8cdc31e3512ab495b7492bb'
+    PATCH = '1'
+    OTHER = 'pre.beta0'
+    BUILD = '069ca569e3f9c28809631a756c8afceffa365091'
   end
-  VERSION = '7.1.0'
-  GEM_VERSION = '7.1.0'
+  VERSION = '7.1.1-beta0'
+  GEM_VERSION = '7.1.1.pre.beta0'
 end
