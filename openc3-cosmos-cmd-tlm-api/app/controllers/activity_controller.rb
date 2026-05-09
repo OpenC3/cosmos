f4953c84224622c465fa0f831bbdd9d7e652ad85
Ryan Melton <ryan@openc3.com>
2026-05-09 12:22:26 -0600

    fix calendar data param

--- openc3-cosmos-cmd-tlm-api/app/controllers/activity_controller.rb
+++ openc3-cosmos-cmd-tlm-api/app/controllers/activity_controller.rb
@@ -72,7 +72,7 @@ class ActivityController < ApplicationController
   # ```
   def create
     return unless authorization('script_run')
-    hash = params.permit(:start, :stop, :kind, :data, :recurring).to_h
+    hash = params.permit(:start, :stop, :kind, :recurring, data: {}).to_h
     begin
       hash['data'] ||= {}
       hash['data']['username'] = username()
@@ -240,7 +240,7 @@ class ActivityController < ApplicationController
   # ```
   def update
     return unless authorization('script_run')
-    hash = params.permit(:start, :stop, :kind, :data).to_h
+    hash = params.permit(:start, :stop, :kind, data: {}).to_h
     begin
       hash['data'] ||= {}
       hash['data']['username'] = username()
