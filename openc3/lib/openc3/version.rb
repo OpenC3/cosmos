77acb91cc2c3b21af3eb981c829285dea96c984e
ryanmelt <184686+ryanmelt@users.noreply.github.com>
2026-06-01 22:13:58 +0000

    [Github Action] Update version to 7.2.0

--- openc3/lib/openc3/version.rb
+++ openc3/lib/openc3/version.rb
@@ -1,14 +1,14 @@
 # encoding: ascii-8bit
 
-OPENC3_VERSION = '7.1.2-beta0'
+OPENC3_VERSION = '7.2.0'
 module OpenC3
   module Version
     MAJOR = '7'
-    MINOR = '1'
-    PATCH = '2'
-    OTHER = 'pre.beta0'
-    BUILD = 'e174a4a6439f28682e74baec89613658fdf9ce5e'
+    MINOR = '2'
+    PATCH = '0'
+    OTHER = ''
+    BUILD = '57bc6819061782287e6e4c866874e13853dc6b40'
   end
-  VERSION = '7.1.2-beta0'
-  GEM_VERSION = '7.1.2.pre.beta0'
+  VERSION = '7.2.0'
+  GEM_VERSION = '7.2.0'
 end
