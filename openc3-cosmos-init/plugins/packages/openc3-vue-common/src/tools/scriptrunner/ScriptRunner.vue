252078d28d7b5e402079de638d6dee789f62fffd
Emily Ragan <emily@openc3.com>
2026-07-28 16:06:26 -0600

    eslint require-await

--- openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/scriptrunner/ScriptRunner.vue
+++ openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/scriptrunner/ScriptRunner.vue
@@ -1673,7 +1673,7 @@ export default {
         }
       }
     }
-    this.updateInterval = setInterval(async () => {
+    this.updateInterval = setInterval(() => {
       this.processReceived()
     }, 100) // Every 100ms
   },
