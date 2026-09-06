593c4c6447087ad6e07585f30e0536fe0fe893b7
Jason Thomas <jason@openc3.com>
2026-09-06 15:50:18 -0600

    Add --no-build to python actions and --proto =https to curl

--- scripts/release/build_multi_arch.sh
+++ scripts/release/build_multi_arch.sh
@@ -73,7 +73,7 @@ fi
 
 # Setup cacert.pem
 echo "Downloading cert from curl"
-curl -q -L https://curl.se/ca/cacert.pem --output ./cacert.pem
+curl -q -L --proto "=https" https://curl.se/ca/cacert.pem --output ./cacert.pem
 if [[ $? -ne 0 ]]; then
   echo "ERROR: Problem downloading cacert.pem file from https://curl.se/ca/cacert.pem" 1>&2
   echo "openc3_setup FAILED" 1>&2
